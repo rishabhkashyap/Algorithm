a38c9ec2086cc66b2e84ffd2e31f9e8d5d574829
rishabhkashyap <kashyap.rishab@hotmail.com>
2017-05-28 22:09:11 +0530

    DFS implementation

---
+++ algortihms/src/com/algo/dfs/DfsMain.java
@@ -0,0 +1,31 @@
+package com.algo.dfs;
+
+import com.graph.model.Vertex;
+
+public class DfsMain {
+
+	public static void main(String[] args) {
+		GraphDfs dfs=new GraphDfs(5);
+		
+		Vertex v1=new Vertex('1');
+		Vertex v2=new Vertex('2');
+		Vertex v3=new Vertex('3');
+		Vertex v4=new Vertex('4');
+		Vertex v5=new Vertex('5');
+		dfs.addVertex(v1);
+		dfs.addVertex(v2);
+		dfs.addVertex(v3);
+		dfs.addVertex(v4);
+		dfs.addVertex(v5);
+		
+		dfs.addEdge(v1, v2);
+		dfs.addEdge(v1, v3);
+		dfs.addEdge(v2, v4);
+		dfs.addEdge(v2, v5);
+		
+		dfs.performDfs(v1);
+		
+
+	}
+
+}
---
+++ algortihms/src/com/algo/dfs/GraphDfs.java
@@ -0,0 +1,62 @@
+package com.algo.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import com.graph.model.Vertex;
+
+public class GraphDfs {
+	private int noOfNodes;
+	private List<Vertex>vertices;
+	private int[][] adjacencyMatrix;
+	public GraphDfs(int noOfNodes) {		
+		this.noOfNodes = noOfNodes;
+		this.vertices = new ArrayList<>();
+		this.adjacencyMatrix = new int[noOfNodes][noOfNodes];
+	}
+	
+	public void addVertex(Vertex vertex){
+		vertices.add(vertex);
+	}
+	
+	public void addEdge(Vertex source,Vertex Detination){
+		int i=vertices.indexOf(source);
+		int j=vertices.indexOf(Detination);
+		adjacencyMatrix[i][j]=1;
+	}
+	
+	private Vertex getAdjacentUnvisitedVertex(Vertex vertex){
+		Vertex neighbour=null;
+		int i=vertices.indexOf(vertex);
+		for(int j=0;j<noOfNodes;j++){
+			if(adjacencyMatrix[i][j]==1 && vertices.get(j).isVisited()==false){
+				neighbour=vertices.get(j);
+				break;
+			}
+		}
+		return neighbour;
+		
+	}
+	
+	public void performDfs(Vertex source){
+		Stack<Vertex>stack=new Stack<>();
+		stack.push(source);
+		source.setVisited(true);
+		System.out.print(source.getLabel()+"\t");
+		while(!stack.isEmpty()){
+			Vertex neighbour=getAdjacentUnvisitedVertex(stack.peek());
+			if(neighbour==null){
+				stack.pop();
+			}else{
+				stack.push(neighbour);
+				neighbour.setVisited(true);
+				System.out.print(neighbour.getLabel()+"\t");
+				
+			}
+		}
+	}
+	
+	
+
+}
