8689c253a0043cad45778e06a686f19592ef9767
rishabhkashyap <kashyap.rishab@hotmail.com>
2017-06-12 00:39:20 +0530

    Queue using one stack

---
+++ algortihms/src/com/stack/QueueUsingOneStack.java
@@ -0,0 +1,29 @@
+package com.stack;
+
+import java.util.Stack;
+
+public class QueueUsingOneStack {
+	private static Stack<Integer> stack = new Stack<>();
+	public static void main(String[] args){
+		eneque(1);
+		eneque(2);
+		stack.pop();
+		eneque(3);
+		eneque(4);
+		while(!stack.isEmpty()){
+			System.out.print(stack.pop()+"\t");
+		}
+		
+	}
+	
+	private static void eneque(int n){
+		if(stack.isEmpty()){
+			stack.push(n);
+		}else{
+			int temp=stack.pop();
+			eneque(n);
+			stack.push(temp);
+		}
+	}
+
+}
