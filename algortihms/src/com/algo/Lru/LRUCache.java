abd8a996832de0bdd80b40b8e239ca36286e0988
rishabhkashyap <kashyap.rishab@hotmail.com>
2017-10-23 18:14:05 +0530

    LRU implementation

---
+++ algortihms/src/com/algo/Lru/LRUCache.java
@@ -0,0 +1,59 @@
+package com.algo.Lru;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class LRUCache {
+	private Map<Integer, Page> pageMap;
+	private List<Integer> pageList;
+	int size;
+	int capacity;
+
+	public LRUCache(int capacity) {
+		this.capacity = capacity;
+		this.size = 0;
+		this.pageList = new LinkedList<>();
+		this.pageMap = new HashMap<>();
+	}
+
+	public void accessPage(int pageNo) {
+		Page page = new Page();
+		//Look for page in map
+		//if its available update page list
+		//remove this page from list and insert it at front
+		if (pageMap.containsKey(pageNo)) {
+			page = pageMap.get(pageNo);
+			updatePageList(page);
+
+		} else {
+			page = new Page(pageNo, "data");
+			if (size < capacity) {
+
+				pageMap.put(pageNo, page);
+				pageList.add(0, pageNo);
+				++size;
+			} else {
+				page = new Page(pageNo, "data");
+				int lastPageNo = pageList.get(pageList.size() - 1);
+				System.out.println("\nRemoving page = "+lastPageNo);
+				pageList.remove(pageList.size() - 1);
+				pageMap.remove(lastPageNo);
+				pageMap.put(pageNo, page);
+				pageList.add(0,pageNo);
+			}
+		}
+		System.out.println();
+		pageList.forEach(e->System.out.print(e+"  "));
+	}
+
+	private void updatePageList(Page page) {
+		int index = pageList.indexOf(page.getPageNo());
+		pageList.remove(index);
+		pageList.add(0, page.getPageNo());
+
+	}
+
+}
---
+++ algortihms/src/com/algo/Lru/Page.java
@@ -0,0 +1,35 @@
+package com.algo.Lru;
+
+public class Page {
+	int pageNo;
+	String data;
+	
+	public Page(int pageNo, String data) {
+		super();
+		this.pageNo = pageNo;
+		this.data = data;
+	}
+	public Page() {}
+	
+	
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	
+	 
+
+}
