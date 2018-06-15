f28d64c8b159aef372e801d337766c75340e1578
rishabh <kashyap.rishab@hotmail.com>
2018-06-15 21:32:25 +0530

    Updated code to remove print statement

--- algortihms/src/com/algo/sorting/SubSort.java
+++ algortihms/src/com/algo/sorting/SubSort.java
@@ -4,7 +4,6 @@
 
 package com.algo.sorting;
 
-
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.OptionalInt;
@@ -14,53 +13,46 @@ import javax.swing.text.StyledEditorKit.ForegroundAction;
 public class SubSort {
 
 	public static void main(String[] args) {
-		int[] arr = { 10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60 };
-		//int[]arr= {0, 1, 15, 25, 6, 7, 30, 40, 50};
+		//int[] arr = { 10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60 };
+		int[] arr = { 5, 7, 9, 13, 16, 12, 15, 8, 14, 18, 17, 20 };
+		int subSortMax = 0;
+		int subSortMin = 0;
 		int leftIndex = getLeftIndex(arr);
 		int rightIndex = getRightIndex(arr);
-		int subSortMax=getMax(arr, leftIndex, rightIndex);
-		int subSortMin=getMin(arr, leftIndex, rightIndex);
-		
-		//once we have max  and min from sub array
-		//we have to check :
-		//1.Is there any element [0,leftIndex] which is greater than subSortMin and update left index accordingly
-		//2.Is there any element [rightIndex,arr.length-1] which is smaller than subSortMax and update right index accordingly
-		
-		leftIndex=updateLeftIndex(arr,0,leftIndex,subSortMin);
-		rightIndex=updateRightIndex(arr,rightIndex, arr.length-1,subSortMax);
-		
+
+		subSortMax = getMax(arr, leftIndex, rightIndex);
+		subSortMin = getMin(arr, leftIndex, rightIndex);
+		// once we have max and min from sub array
+		// we have to check :
+		// 1.Is there any element [0,leftIndex] which is greater than subSortMin and
+		// update left index accordingly
+		// 2.Is there any element [rightIndex,arr.length-1] which is smaller than
+		// subSortMax and update right index accordingly
+
+		leftIndex = updateLeftIndex(arr, 0, leftIndex, subSortMin);
+		rightIndex = updateRightIndex(arr, rightIndex, arr.length - 1, subSortMax);
+
 		System.out.println(" \nIndex begin = " + leftIndex + "\t Index end = " + rightIndex);
 	}
 
-
 	private static int updateRightIndex(int[] arr, int rightIndex, int end, int subSortMax) {
-		
-		for(int i=rightIndex+1;i<=end;i++) {
-			if(arr[i]<subSortMax) {
-				rightIndex=i;
+
+		for (int i = rightIndex + 1; i <= end; i++) {
+			if (arr[i] < subSortMax) {
+				rightIndex = i;
 			}
-		}	
+		}
 		return rightIndex;
 	}
 
-
 	private static int updateLeftIndex(int[] arr, int begin, int leftIndex, int subSortMin) {
-		for(int i=begin;i<=leftIndex;i++ ) {
-			if(arr[i]>subSortMin) {
-				leftIndex=i;
+		for (int i = begin; i < leftIndex; i++) {
+			if (arr[i] > subSortMin) {
+				leftIndex = i;
 			}
 		}
 		return leftIndex;
 	}
-
-
-
-
-
-
-
-
-
 
 	private static int getLeftIndex(int[] arr) {
 		int leftIndex = 0;
@@ -70,8 +62,6 @@ public class SubSort {
 				break;
 			}
 		}
-		--leftIndex;
-		System.out.println("left index = " + leftIndex);
 		return leftIndex;
 	}
 
@@ -84,23 +74,19 @@ public class SubSort {
 			}
 
 		}
-		System.out.println("right index = " + rightIndex);
 		return rightIndex;
 	}
-	
-	private static int getMax(int[] arr,int leftMin,int rightMax) {
-		OptionalInt maxElementOpt=null;	
-		maxElementOpt=Arrays.stream(arr,leftMin,rightMax)
-				         .max();
-				         
-		
+
+	private static int getMax(int[] arr, int leftMin, int rightMax) {
+		OptionalInt maxElementOpt = null;
+		maxElementOpt = Arrays.stream(arr, leftMin, rightMax).max();
+
 		return maxElementOpt.getAsInt();
 	}
-	
-	private static int getMin(int[] arr,int leftMin,int rightMax) {
-		OptionalInt minElementOpt=null;
-		minElementOpt=Arrays.stream(arr, leftMin, rightMax)
-				            .min();
+
+	private static int getMin(int[] arr, int leftMin, int rightMax) {
+		OptionalInt minElementOpt = null;
+		minElementOpt = Arrays.stream(arr, leftMin, rightMax).min();
 		return minElementOpt.getAsInt();
 	}
 
