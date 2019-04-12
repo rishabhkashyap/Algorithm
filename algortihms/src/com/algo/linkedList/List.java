d15c77962a6a605c059c0653344e3a0847bf9c3a
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-04-12 07:34:13 +0530

    Adding method to set head of list

--- algortihms/src/com/algo/linkedList/List.java
+++ algortihms/src/com/algo/linkedList/List.java
@@ -1,60 +1,65 @@
 package com.algo.linkedList;
 
 public class List {
-	private Node head;
-	private Node headPrev;
-	
+    private Node head;
+    private Node headPrev;
 
-	public Node getHead() {
-		return head;
-	}
+    public Node getHead() {
+        return head;
+    }
 
-	public Node getHeadPrev() {
-		return headPrev;
-	}
+    public void setHead(Node head) {
+        this.head = head;
+    }
 
-	// Adds element at front of linked list
-	public void add(Node node) {
-		if (head == null) {
-			head = node;
-		} else {
-			node.next = head;
-			head = node;
-		}
+    public Node getHeadPrev() {
+        return headPrev;
+    }
 
-	}
-	
-	//Adds node at end of list
-	public void addLast(Node node){
-		if(head==null){
-			head=node;
-		}else{
-			Node temp=head;
-			while (temp.next!=null){
-				temp=temp.next;
-			}
-			temp.next=node;
-		}
-	}
+    // Adds element at front of linked list
+    public void add(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
 
-	
+    }
 
-	public void display() {
-		Node temp = head;
-		while (temp != null) {
-			System.out.print(temp.value + "\t");
-			temp = temp.next;
-		}
+    //Adds node at end of list
+    public void addLast(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
 
-	}
-	public void display(Node head) {
-		Node temp = head;
-		while (temp != null) {
-			System.out.print(temp.value + "\t");
-			temp = temp.next;
-		}
 
-	}
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + "\t");
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
+
+    public void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + "\t");
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
 
 
 }
