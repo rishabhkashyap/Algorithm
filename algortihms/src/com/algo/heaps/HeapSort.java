b7ac23866c402f4f3d56d82bcf282aa51138e61c
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-07-11 07:01:04 +0530

    code formatting

--- algortihms/src/com/algo/heaps/HeapSort.java
+++ algortihms/src/com/algo/heaps/HeapSort.java
@@ -1,77 +1,75 @@
-
 package com.algo.heaps;
 
-	
 
 public class HeapSort {
 
-	private int[] arr;
-	int size;
+    int size;
+    private int[] arr;
 
-	public HeapSort(int[] arr) {
-		super();
-		this.arr = arr;
-		this.size = arr.length;
-	}
+    public HeapSort(int[] arr) {
+        super();
+        this.arr = arr;
+        this.size = arr.length;
+    }
 
-	public static void main(String[] args) {
-		int[]arr={5,4,3,2,1};
-		HeapSort hs=new HeapSort(arr);
-		System.out.println("Unsorted array :");
-		hs.displayArr();
-		hs.sort();
-		System.out.println("\nSorted array :");
-		hs.displayArr();
-		
-	}
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        HeapSort hs = new HeapSort(arr);
+        System.out.println("Unsorted array :");
+        hs.displayArr();
+        hs.sort();
+        System.out.println("\nSorted array :");
+        hs.displayArr();
 
-	public void sort() {
-		for (int i = size / 2 - 1; i >= 0; i--) {
-			heapify(i);
-		}
-		for (int i = arr.length-1; i >= 0; i--) {
-			int temp = arr[0];
-			arr[0] = arr[size - 1];
-			arr[size - 1] = temp;
-			--size;
-			heapify(0);
-		}
+    }
 
-	}
+    public void sort() {
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(i);
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[size - 1];
+            arr[size - 1] = temp;
+            --size;
+            heapify(0);
+        }
 
-	private void heapify(int i) {
-		int leftChildIndex = getLeftChildIndex(i);
-		int rightChildIndex = getRightChildIndex(i);
-		int largestIndex = i;
-		if (leftChildIndex < size && arr[leftChildIndex] > arr[largestIndex]) {
-			largestIndex = leftChildIndex;
-		}
-		if (rightChildIndex < size && arr[rightChildIndex] > arr[largestIndex]) {
-			largestIndex = rightChildIndex;
-		}
-		if (largestIndex != i) {
-			int swap = arr[i];
-			arr[i] = arr[largestIndex];
-			arr[largestIndex] = swap;
-			// Recursively heapify affected subtree
-			heapify(largestIndex);
-		}
+    }
 
-	}
+    private void heapify(int i) {
+        int leftChildIndex = getLeftChildIndex(i);
+        int rightChildIndex = getRightChildIndex(i);
+        int largestIndex = i;
+        if (leftChildIndex < size && arr[leftChildIndex] > arr[largestIndex]) {
+            largestIndex = leftChildIndex;
+        }
+        if (rightChildIndex < size && arr[rightChildIndex] > arr[largestIndex]) {
+            largestIndex = rightChildIndex;
+        }
+        if (largestIndex != i) {
+            int swap = arr[i];
+            arr[i] = arr[largestIndex];
+            arr[largestIndex] = swap;
+            // Recursively heapify affected subtree
+            heapify(largestIndex);
+        }
 
-	private int getRightChildIndex(int i) {
-		return 2 * i + 2;
-	}
+    }
 
-	private int getLeftChildIndex(int i) {
-		return 2 * i + 1;
-	}
+    private int getRightChildIndex(int i) {
+        return 2 * i + 2;
+    }
 
-	public void displayArr() {
-		System.out.println();
-		for (int i : arr) {
-			System.out.print(i + " ");
-		}
-	}
+    private int getLeftChildIndex(int i) {
+        return 2 * i + 1;
+    }
+
+    public void displayArr() {
+        System.out.println();
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 
 }
