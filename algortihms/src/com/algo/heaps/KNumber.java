5559ed05872e6738eceb83ac9df555ee752da982
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-07-19 06:33:43 +0530

    Code refactoring:adding new variable to store kth smallest element

--- algortihms/src/com/algo/heaps/KNumber.java
+++ algortihms/src/com/algo/heaps/KNumber.java
@@ -7,22 +7,23 @@ public class KNumber {
 
     public static void main(String[] args) {
         int[] arr = {7, 10, 4, 3, 20, 15};
-        int k = 4;
+        int k = 3;
         int kSmallest = findKthSmallestElement(arr, k);
         System.out.println(k + "th smallest element = " + kSmallest);
     }
 
     private static int findKthSmallestElement(int[] arr, int k) {
         Queue<Integer> queue = new PriorityQueue<>();
+        int kthSmallest = Integer.MAX_VALUE;
 
         for (int element : arr) {
             queue.add(element);
         }
         int i = 0;
-        while (!queue.isEmpty() && i < k-1) {
-            queue.remove();
+        while (!queue.isEmpty() && i < k) {
+            kthSmallest = queue.remove();
             ++i;
         }
-        return queue.remove();
+        return kthSmallest;
     }
 }
