f5cc75a06690ffe7f72eeff2f97d4231af598503
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-08-12 06:07:53 +0530

    Bubble sort using recursion

--- algortihms/src/com/algo/sorting/BubbleSort.java
+++ algortihms/src/com/algo/sorting/BubbleSort.java
@@ -2,25 +2,40 @@ package com.algo.sorting;
 
 public class BubbleSort {
 
-	public static void main(String[] args) {
-		int[]arr={5,2,1,10,6,9};
-		sort(arr);
-		for(int i:arr){
-			System.out.print(i+"  ");
-		}
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 1, 10, 6, 9};
+        sortRecurseive(arr, arr.length);
+        for (int i : arr) {
+            System.out.print(i + "  ");
+        }
 
-	}
-	
-	private  static void sort(int[]a){
-		for(int i=0;i<a.length-1;i++){
-			for(int j=0;j<a.length-i-1;j++){
-				if(a[j]<a[j+1]){
-					int temp=a[j];
-					a[j]=a[j+1];
-					a[j+1]=temp;
-				}
-			}
-		}
-	}
+    }
+
+    private static void sort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] < a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void sortRecurseive(int[] a, int n) {
+
+        for (int j = 0; j < n - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+            if (n - 1 > 1) {
+                sortRecurseive(a, n - 1);
+            }
+        }
+
+    }
 
 }
