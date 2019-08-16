e4b9020b71fab06038473d5a7810c8b8bd53de28
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-08-16 06:11:16 +0530

    Iterative quicksort

--- algortihms/src/com/algo/sorting/quicksort/QuickSort.java
+++ algortihms/src/com/algo/sorting/quicksort/QuickSort.java
@@ -1,45 +1,85 @@
 package com.algo.sorting.quicksort;
 
+import java.util.Stack;
+
 public class QuickSort {
-	private int size;
-	private int [] numbers;
-	
-	public QuickSort(int[] numbers){
-		this.numbers=numbers;
-		this.size=numbers.length;
-	}
-	
-	public void doQuicksort(int low,int high){
-		int i=low;
-		int j=high;
-		int pivot=numbers[low+(high-low)/2];
-		
-		while(i<=j){
-			while(numbers[i]<pivot){
-				++i;
-			}
-			while(numbers[j]>pivot){
-				--j;
-			}
-			if(i<=j){
-				exchange(i,j);
-				++i;
-				--j;
-			}
-		}
-		if(low<j){
-			doQuicksort(low,j);
-		}
-		if(high>i){
-			doQuicksort(i,high);
-		}
-				
-	}
-	
-	private  void exchange(int i,int j){
-		int temp=numbers[i];
-		numbers[i]=numbers[j];
-		numbers[j]=temp;
-	}
+    //private int size;
+    private int[] numbers;
+
+    public QuickSort(int[] numbers) {
+        this.numbers = numbers;
+        // this.size = numbers.length;
+    }
+
+    public void doQuicksort(int low, int high) {
+        int i = low;
+        int j = high;
+        int pivot = numbers[low + (high - low) / 2];
+
+        while (i <= j) {
+            while (numbers[i] < pivot) {
+                ++i;
+            }
+            while (numbers[j] > pivot) {
+                --j;
+            }
+            if (i <= j) {
+                exchange(i, j);
+                ++i;
+                --j;
+            }
+        }
+        if (low < j) {
+            doQuicksort(low, j);
+        }
+        if (high > i) {
+            doQuicksort(i, high);
+        }
+
+    }
+
+    public void iterativeQuickSort() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        stack.push(this.numbers.length - 1);
+        while (!stack.isEmpty()) {
+            int high = stack.pop();
+            int low = stack.pop();
+
+
+            int partitionIndex = partition(low, high);
+            if (partitionIndex - 1 > low) {
+                stack.push(low);
+                stack.push(partitionIndex - 1);
+
+            }
+            if (partitionIndex + 1 < high) {
+                stack.push(partitionIndex + 1);
+                stack.push(high);
+
+            }
+
+
+        }
+    }
+
+    private int partition(int low, int high) {
+        int pivot = numbers[high];
+        int partitionIndex = low;
+        for (int i = low; i < high; i++) {
+            if (numbers[i] < pivot) {
+                exchange(i, partitionIndex);
+                ++partitionIndex;
+            }
+        }
+        exchange(partitionIndex, high);
+        return partitionIndex;
+    }
+
+    private void exchange(int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
 
 }
--- algortihms/src/com/algo/sorting/quicksort/QuickSortMain.java
+++ algortihms/src/com/algo/sorting/quicksort/QuickSortMain.java
@@ -2,14 +2,17 @@ package com.algo.sorting.quicksort;
 
 public class QuickSortMain {
 
-	public static void main(String[] args) {
-		int arr[]={5,4,6,3,1,8,23};
-		QuickSort quick=new QuickSort(arr);
-		quick.doQuicksort(0, arr.length-1);
-		for(int num:arr){
-			System.out.print(num+"\t");
-		}
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 6, 3, 1, 8, 23};
+        //int arr[] = {34, 31, 43, 12, 11, 30, 22, 21, 32};
+        //int arr[] = {10, 7, 8, 9, 1, 5};
+        QuickSort quick = new QuickSort(arr);
+        //quick.doQuicksort(0, arr.length - 1);
+        quick.iterativeQuickSort();
+        for (int num : arr) {
+            System.out.print(num + "\t");
+        }
 
-	}
+    }
 
 }
