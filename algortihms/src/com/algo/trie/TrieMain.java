00e13333c8b25fc16a629fa32d0193140e3334c4
Rishabh Kashyap <kashyap.rishab91@gmail.com>
2019-08-20 12:16:05 +0530

    trie update

--- algortihms/src/com/algo/trie/TrieMain.java
+++ algortihms/src/com/algo/trie/TrieMain.java
@@ -4,11 +4,12 @@ public class TrieMain {
 
 	public static void main(String[] args) {
 		Trie trie = new Trie();
-		
+        trie.insert("apple");
 		trie.insert("geek");
 		trie.insert("geekforgeeks");
 		trie.insert("geeky");
 		trie.insert("geektrust");
+
 		
 		System.out.println(" Apple is available = "+trie.search("apple"));
 		System.out.println("Longest common prefix = "+ trie.getLongestPrefix());
