704ebe0c18e17427b01f36789798c3aac097fc35
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-10-03 05:39:43 +0530

    find missing element in array of size n and contains element from 1to n

---
+++ algortihms/src/com/algo/search/MissingElement.java
@@ -0,0 +1,42 @@
+package com.algo.search;
+
+public class MissingElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 6, 3, 7, 8};
+        int[] arr1 = {1, 2, 2, 6, 3, 7, 8};
+        int missingElelemt = getMissingElement(arr);
+        System.out.println("Get missing element = " + missingElelemt);
+        missingElelemt = getmissingElelementDuplicate(arr1);
+        System.out.println("Get missing element = " + missingElelemt);
+    }
+
+    private static int getMissingElement(int[] arr) {
+        int x1 = 1;
+        int x2 = arr[0];
+        for (int i = 2; i <= arr.length + 1; i++) {
+            x1 = x1 ^ i;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            x2 = x2 ^ arr[i];
+        }
+        return x1 ^ x2;
+    }
+
+    private static int getmissingElelementDuplicate(int[] arr) {
+        int missingNumber = -1;
+        boolean[] isAvailable = new boolean[20];
+        for (int i = 0; i < arr.length; i++) {
+            isAvailable[arr[i]] = true;
+        }
+
+        for (int i = 1; i < isAvailable.length; i++) {
+            if (isAvailable[i] == false) {
+                missingNumber = i;
+                break;
+            }
+        }
+        return missingNumber;
+    }
+
+
+}
