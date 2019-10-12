ed9507336fe07ab61173c6a0a162b3123d5a335d
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-10-12 07:35:32 +0530

    Print top view of tree using iterative method.

--- algortihms/src/com/algo/tree/traversal/TopView.java
+++ algortihms/src/com/algo/tree/traversal/TopView.java
@@ -17,9 +17,12 @@ public class TopView {
         node2.setLeft(node4);
         node2.setRight(node5);
         node3.setRight(node6);
-        node6.setRight(new Node(21));
+        node3.setLeft(new Node(10));
+        node6.setRight(new Node(7));
 
         printTopView(root);
+        System.out.println();
+        printTopIterative(root);
     }
 
     private static void printTopView(Node root) {
@@ -40,5 +43,45 @@ public class TopView {
         }
         printTopViewHelper(root.getLeft(), nodeMap, i - 1);
         printTopViewHelper(root.getRight(), nodeMap, i + 1);
+    }
+
+    private static void printTopIterative(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<QueueNode> queue = new LinkedList<>();
+        Map<Integer, Node> map = new HashMap<>();
+        QueueNode queueNode = new QueueNode(root, 0);
+        queue.add(queueNode);
+        while (!queue.isEmpty()) {
+            QueueNode currentNode = queue.remove();
+            if (!map.containsKey(currentNode.hd)) {
+                map.put(currentNode.hd, currentNode.node);
+            }
+            if (currentNode.node.getLeft() != null) {
+                queue.add(new QueueNode(currentNode.node.getLeft(), currentNode.hd - 1));
+            }
+            if (currentNode.node.getRight() != null) {
+                queue.add(new QueueNode(currentNode.node.getRight(), currentNode.hd + 1));
+            }
+
+
+        }
+
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        keys.forEach(k -> System.out.print(map.get(k).getData() + "\t"));
+
+
+    }
+
+    private static class QueueNode {
+        private Node node;
+        private int hd;
+
+        public QueueNode(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
     }
 }
