0833cff24074dfbee5ce3e4ab0a207a53733e034
rishabhkashyap <kashyap.rishab@hotmail.com>
2019-12-03 05:34:24 +0530

    Updating condition to fill zeroes in array

--- algortihms/src/com/algo/sorting/RemoveDuplicates.java
+++ algortihms/src/com/algo/sorting/RemoveDuplicates.java
@@ -6,24 +6,27 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
-        System.out.println("Before sorting");
+       // int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println("Before removing duplicates");
         Arrays.stream(arr).forEach(e -> System.out.print(e + "\t"));
         removeDuplicates(arr);
-        System.out.println("\nArray after sorting");
+        System.out.println("\n\nAfter removing duplicates");
         Arrays.stream(arr).forEach(e -> System.out.print(e + "\t"));
     }
 
     private static void removeDuplicates(int[] arr) {
         int j = 1;
-        for (int i = 1; i < arr.length-1; i++) {
-            if (arr[j-1] != arr[i]) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[j - 1] != arr[i]) {
                 arr[j] = arr[i];
                 ++j;
             }
         }
-        while (j<arr.length){
-            arr[j]=0;
-            ++j;
+        if (j + 1 < arr.length) {
+            while (j < arr.length) {
+                arr[j] = 0;
+                ++j;
+            }
         }
     }
 }
