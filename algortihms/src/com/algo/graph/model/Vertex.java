6145c66b0443e4ec0af52625174b9ec667799e5d
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-01-22 07:03:33 +0530

    Updating Vertex class to add colour of vertex

--- algortihms/src/com/algo/graph/model/Vertex.java
+++ algortihms/src/com/algo/graph/model/Vertex.java
@@ -5,12 +5,19 @@ import java.util.Objects;
 public class Vertex {
     private String label;
     private boolean visited;
+    private String colour;
 
 
     public Vertex(String label) {
         super();
         this.label = label;
         this.visited = false;
+    }
+
+    public Vertex(String label, boolean visited, String colour) {
+        this.label = label;
+        this.visited = visited;
+        this.colour = colour;
     }
 
     public String getLabel() {
@@ -27,6 +34,14 @@ public class Vertex {
 
     public void setVisited(boolean visited) {
         this.visited = visited;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
     }
 
     @Override
