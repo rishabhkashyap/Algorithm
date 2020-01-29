3a59b4cd1a2fa3292577e0c72b321ec193b9d9b3
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-01-29 05:57:17 +0530

    rotate array 90 degree clock wise

---
+++ algortihms/src/com/algo/arrays/Rotate90.java
@@ -0,0 +1,32 @@
+package com.algo.arrays;
+
+public class Rotate90 {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+        rotateMatrix(arr);
+    }
+
+    private static void rotateMatrix(int[][] arr) {
+        int n = arr.length;
+        for (int i = 0; i < arr.length / 2; i++) {
+            for (int j = i; j < arr.length - i - 1; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[n - 1 - j][i];
+                arr[n - 1 - j][i] = arr[n - 1 - i][n - 1 - j];
+                arr[n - 1 - i][n - 1 - j] = arr[j][n - 1 - i];
+                arr[j][n - 1 - i] = temp;
+
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
