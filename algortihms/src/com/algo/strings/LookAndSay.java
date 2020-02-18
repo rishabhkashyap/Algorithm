10b81bf96e5f5ef28d03674207c538e172c7855a
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-02-19 05:15:35 +0530

    Print look and say series(1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211, ...)

---
+++ algortihms/src/com/algo/strings/LookAndSay.java
@@ -0,0 +1,56 @@
+package com.algo.strings;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LookAndSay {
+    public static void main(String[] args) {
+        printSeries(7);
+    }
+
+    private static void printSeries(int n) {
+        String next = "1";
+        for (int i = 0; i < n; i++) {
+            System.out.print(next + "\t");
+            next = nextNumber(next);
+        }
+    }
+
+    private static String nextNumber(String next) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 1;
+        if (next.length() == 1) {
+            stringBuilder.append(1);
+            stringBuilder.append(next.charAt(0));
+        } else {
+            for (int i = 0; i < next.length(); i++) {
+                if (i + 1 < next.length() && next.charAt(i) == next.charAt(i + 1)) {
+                    ++count;
+                } else {
+                    stringBuilder.append(count);
+                    stringBuilder.append(next.charAt(i));
+                    count = 1;
+                }
+            }
+
+        }
+
+        return stringBuilder.toString();
+
+    }
+
+
+    private static Map<Character, Integer> getCountMap(String next) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < next.length(); i++) {
+            if (map.containsKey(next.charAt(i))) {
+                int count = map.get(next.charAt(i));
+                ++count;
+                map.put(next.charAt(i), count);
+            } else {
+                map.put(next.charAt(i), 1);
+            }
+        }
+        return map;
+    }
+}
