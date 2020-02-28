9c7702e3ccacf23b9f36cb26a588f185fc6b48fe
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-02-28 08:14:52 +0530

    Find length of longest sub array with sum as zero

---
+++ algortihms/src/com/algo/hashing/MaxLenSubarrayWithSumZero.java
@@ -0,0 +1,36 @@
+package com.algo.hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxLenSubarrayWithSumZero {
+
+    public static void main(String[] args) {
+        int[] arr = {15, -2, 2, -8, 1, 7, 10, 23};
+        //int[] arr = {1, 0, 3};
+        //int[] arr={1,9,3,4,5};
+        System.out.println("Longest sub array with sum zero = " + findMaxLenSubArrayWithSumZero(arr));
+    }
+
+    private static int findMaxLenSubArrayWithSumZero(int[] arr) {
+        int maxLen = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+            if (prefixSum == 0 && maxLen == 0) {
+                maxLen = 1;
+            }
+            if (prefixSum == 0) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+            if (map.containsKey(prefixSum)) {
+                int index = map.get(prefixSum);
+                maxLen = Math.max(maxLen, i - index);
+            } else {
+                map.put(prefixSum, i);
+            }
+        }
+        return maxLen;
+    }
+}
