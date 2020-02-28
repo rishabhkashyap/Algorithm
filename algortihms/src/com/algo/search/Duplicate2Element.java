80eda189ef4b06622b8c91221f8132e4c5b36e6e
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-02-29 05:08:31 +0530

    Find duplicate values by sorting

--- algortihms/src/com/algo/search/Duplicate2Element.java
+++ algortihms/src/com/algo/search/Duplicate2Element.java
@@ -1,5 +1,7 @@
 package com.algo.search;
 
+import java.util.Arrays;
+
 public class Duplicate2Element {
     public static void main(String[] args) {
         int[] arr = {4, 2, 4, 5, 2, 3, 1};
@@ -7,6 +9,8 @@ public class Duplicate2Element {
         printDuplicateElements(arr, n);
         System.out.println();
         printDuplicate(arr);
+        System.out.println("\nFind Duplicates using sorting");
+        findDuplicatesBySorting(arr);
 
     }
 
@@ -47,6 +51,21 @@ public class Duplicate2Element {
         for (int i = 0; i < arr.length; ++i) {
             if (freq[i] >= 2) {
                 System.out.println("Repeated number1 = " + i);
+            }
+        }
+    }
+
+    private static void findDuplicatesBySorting(int[] arr) {
+        Arrays.sort(arr);
+        int count = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (i + 1 < arr.length && arr[i] == arr[i + 1]) {
+                ++count;
+                if (count > 2) {
+                    System.out.println("Repeated number = " + arr[i]);
+                }
+            } else {
+                count = 1;
             }
         }
     }
