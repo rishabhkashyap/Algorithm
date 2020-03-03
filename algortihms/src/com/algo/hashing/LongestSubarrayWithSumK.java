9f081e3a0677b338a6b1edf20f4c9193c372a6c0
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-03-03 07:11:17 +0530

    Find max length of subarray whose sum is equal to zero

---
+++ algortihms/src/com/algo/hashing/LongestSubarrayWithSumK.java
@@ -0,0 +1,38 @@
+package com.algo.hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubarrayWithSumK {
+    public static void main(String[] args) {
+        //int[] arr = {10, 5, 2, 7, 1, 9};
+        //int[] arr = {1, -1, 5, -2, 3};
+        int[] arr = {-5, 8, -14, 2, 4, 12};
+        //int k = 15;
+        //int k = 3;
+        int k = -5;
+        System.out.println("Longest sub array length = " + findLongestSubArray(arr, k));
+    }
+
+    private static int findLongestSubArray(int[] arr, int target) {
+        int maxLen = 0;
+        int prefixSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+            if (arr[i] == target && maxLen == 0) {
+                maxLen = 1;
+            }
+            if (prefixSum == target) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+            if (map.containsKey(prefixSum - target)) {
+                maxLen = Math.max(maxLen, i - map.get(prefixSum - target));
+            } else {
+                map.put(prefixSum, i);
+            }
+
+        }
+        return maxLen;
+    }
+}
