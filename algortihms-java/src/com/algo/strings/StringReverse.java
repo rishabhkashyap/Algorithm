129f1bd6f15c44ddb921067c621b3cae3572cd25
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-08-20 05:16:16 +0530

    Adding alternate approach to reverse a string using recursion

--- algortihms-java/src/com/algo/strings/StringReverse.java
+++ algortihms-java/src/com/algo/strings/StringReverse.java
@@ -2,16 +2,23 @@ package com.algo.strings;
 
 public class StringReverse {
 
-    public static void main(String[] args){
-        String string="qwerty";
-        string=reverse(string);
-        System.out.println("Reversed string = "+string);
+    public static void main(String[] args) {
+        String string = "qwerty";
+        System.out.println("Reversed string = " + reverse(string));
+        System.out.println("Reversed string = " + reverse2(string));
     }
 
     private static String reverse(String string) {
-        if(string.length()==0||string.length()==1){
+        if (string.length() == 0 || string.length() == 1) {
             return string;
         }
-        return string.charAt(string.length()-1)+reverse(string.substring(0,string.length()-1));
+        return string.charAt(string.length() - 1) + reverse(string.substring(0, string.length() - 1));
+    }
+
+    private static String reverse2(String string) {
+        if (string.length() == 1) {
+            return string;
+        }
+        return reverse(string.substring(1)) + string.charAt(0);
     }
 }
