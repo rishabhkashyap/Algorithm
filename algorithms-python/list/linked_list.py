a31edf6b1c7c1fe278880f9ab61006903ae5d3ca
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-09-06 05:25:50 +0530

    Adding methods to add element at end and search element in list

--- algorithms-python/list/linked_list.py
+++ algorithms-python/list/linked_list.py
@@ -39,10 +39,27 @@ class LinkedList:
         node.next = self._head
         self._head = node
 
+    def add_last(self, node: Node):
+        if self._head is None:
+            self._head = node
+            return
+        temp: Node = self._head
+        while temp.next is not None:
+            temp = temp.next
+        temp.next = node
+
+    def search(self, element: int) -> bool:
+        temp: Node = self._head
+        while temp is not None:
+            if temp.data == element:
+                return True
+            temp = temp.next
+        return False
+
     def display(self):
         temp: Node = self._head
         while temp is not None:
-            print(temp.data, end="\t",)
+            print(temp.data, end="\t", )
             temp = temp.next
 
 
@@ -50,8 +67,21 @@ if __name__ == '__main__':
     node1: Node = Node(1)
     node2: Node = Node(2)
     node3: Node = Node(3)
-    linked_list: LinkedList = LinkedList()
-    linked_list.add_first(node1)
-    linked_list.add_first(node2)
-    linked_list.add_first(node3)
-    linked_list.display()
+    linked_list1: LinkedList = LinkedList()
+    print("Inserting elements at beginning")
+    linked_list1.add_first(node1)
+    linked_list1.add_first(node2)
+    linked_list1.add_first(node3)
+    linked_list1.display()
+    print("\n*****************************************************")
+    linked_list2: LinkedList = LinkedList()
+    node1.next = None
+    node2.next = None
+    node3.next = None
+    print("Inserting element at end")
+    linked_list2.add_last(node1)
+    linked_list2.add_last(node2)
+    linked_list2.add_last(node3)
+    linked_list2.display()
+    print(f"\nList contains element 1 = {linked_list2.search(1)}")
+    print(f"List contains element 8 = {linked_list2.search(8)}")
