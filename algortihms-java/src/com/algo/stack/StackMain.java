fa9430f3b0e87cc963d09d25c3912881c55f6b3e
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-09-10 05:19:52 +0530

    Updating main class to remove duplicate code

--- algortihms-java/src/com/algo/stack/StackMain.java
+++ algortihms-java/src/com/algo/stack/StackMain.java
@@ -2,7 +2,15 @@ package com.algo.stack;
 
 public class StackMain {
     public static void main(String[] args) {
+        System.out.println("\n**************Array implementation****************");
         Stack stack = new StackArr(5);
+        doStackOperation(stack);
+        System.out.println("\n**************List implementation****************");
+        stack = new StackList();
+        doStackOperation(stack);
+    }
+
+    private static void doStackOperation(Stack stack){
         stack.push(1);
         stack.push(2);
         stack.push(3);
@@ -15,5 +23,8 @@ public class StackMain {
         System.out.println();
         stack.display();
         System.out.println("\nElement at top = " + stack.peek());
+        System.out.println("Total elements in stack = "+stack.size());
+        System.out.println("Is stack empty = "+stack.isEmpty());
+
     }
 }
