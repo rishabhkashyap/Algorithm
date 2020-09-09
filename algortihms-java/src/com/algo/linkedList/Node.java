c14a26886d0b41f9ed719bc9066e16af8f56af5a
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-09-10 05:09:14 +0530

    Adding getter/setter methods

--- algortihms-java/src/com/algo/linkedList/Node.java
+++ algortihms-java/src/com/algo/linkedList/Node.java
@@ -1,27 +1,31 @@
 package com.algo.linkedList;
 
 public class Node {
-	public int value;
-	public Node next;
-	public int getValue() {
-		return value;
-	}
-	public Node(){
-		
-	}
-	public Node(int value) {
-		this.value = value;
-		this.next = null;
-	}
-	
-//	public void setValue(int value) {
-//		this.value = value;
-//	}
-//	public Node getNext() {
-//		return next;
-//	}
-//	public void setNext(Node next) {
-//		this.next = next;
-//	}
+    public int value;
+    public Node next;
 
+    public Node() {
+
+    }
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
