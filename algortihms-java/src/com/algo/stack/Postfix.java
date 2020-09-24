fd1a2a16b8ec5b16ca52e964465832df75d130db
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-09-24 05:39:23 +0530

    Infix to postfix conversion

---
+++ algortihms-java/src/com/algo/stack/Postfix.java
@@ -0,0 +1,72 @@
+package com.algo.stack;
+
+import java.util.Stack;
+
+public class Postfix {
+    public static void main(String[] args) {
+        String infixExpression = "a+b*(c^d-e)^(f+g*h)-i";
+        //String infixExpression = "a+b";
+        System.out.println("Postfix expression = " + convertToPostfix(infixExpression));
+    }
+
+    private static String convertToPostfix(String infixExpression) {
+        StringBuilder result = new StringBuilder("");
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < infixExpression.length(); i++) {
+            char ch = infixExpression.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                result.append(ch);
+            } else if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                removeFromStack(stack, result);
+            } else {
+                addOperandToStack(ch, stack, result);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.toString();
+    }
+
+    private static void removeFromStack(Stack<Character> stack, StringBuilder result) {
+        while (!stack.isEmpty() && stack.peek() != '(') {
+            result.append(stack.pop());
+        }
+        if (!stack.isEmpty() && stack.peek() == '(') {
+            stack.pop();
+        }
+
+    }
+
+    private static void addOperandToStack(char ch, Stack<Character> stack, StringBuilder result) {
+        while (!stack.isEmpty() && getPrcedence(ch) <= getPrcedence(stack.peek())) {
+            result.append(stack.pop());
+        }
+        stack.push(ch);
+    }
+
+
+    private static int getPrcedence(Character operator) {
+
+        int preference = -1;
+
+        switch (operator) {
+            case '+':
+            case '-':
+                preference = 1;
+                break;
+            case '*':
+            case '/':
+                preference = 2;
+                break;
+            case '^':
+                preference = 3;
+                break;
+            default:
+                preference = 0;
+        }
+        return preference;
+    }
+}
