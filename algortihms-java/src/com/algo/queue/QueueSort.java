710afc8c6ca0b8c9553d64f9bc3f2c8ba52f2f85
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-10-28 05:31:21 +0530

    Sort queue using queue operation and without using auxiliary space

--- algortihms-java/src/com/algo/queue/QueueSort.java
+++ algortihms-java/src/com/algo/queue/QueueSort.java
@@ -7,44 +7,60 @@ import java.util.Queue;
 public class QueueSort {
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<>();
+//        queue.add(5);
+//        queue.add(29);
+//        queue.add(10);
+//        queue.add(3);
+//        queue.add(8);
+//        queue.add(1);
+//        queue.add(7);
+
+        queue.add(11);
         queue.add(5);
-        queue.add(29);
-        queue.add(10);
-        queue.add(3);
-        queue.add(8);
-        queue.add(1);
-        queue.add(7);
+        queue.add(4);
+        queue.add(21);
         queue = sort(queue);
-        for (int element : queue) {
-            System.out.print(element + "\t");
+        while (!queue.isEmpty()) {
+            System.out.print(queue.remove() + "\t");
         }
 
 
     }
 
     private static Queue<Integer> sort(Queue<Integer> queue) {
-        Queue<Integer> sortedQueue = new LinkedList<>();
-        //Queue<Integer> tempQueue=new LinkedList<>();
-        int minElement = Integer.MAX_VALUE;
-        int counter = 0;
-        while (!queue.isEmpty()) {
+        for (int i = 1; i <= queue.size(); i++) {
+            int minIndex = findMinIndex(queue, queue.size() - i);
+            insert(queue, minIndex);
+        }
+        return queue;
+    }
+
+    private static int findMinIndex(Queue<Integer> queue, int index) {
+        int minIndex = -1;
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < queue.size(); i++) {
             int element = queue.remove();
-            if (!sortedQueue.isEmpty()) {
-                while (!sortedQueue.isEmpty()) {
+            if (element <= minValue && i <= index) {
+                minIndex = i;
+                minValue = element;
+            }
+            queue.add(element);
+        }
+        return minIndex;
+    }
 
-                    if (sortedQueue.peek() > element) {
-                        sortedQueue.add(sortedQueue.remove());
-                    } else {
-                        queue.add(sortedQueue.remove());
-                    }
-                }
-                sortedQueue.add(element);
-
+    private static void insert(Queue<Integer> queue, int minIndex) {
+        int minValue = 0;
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            int element = queue.remove();
+            if (i == minIndex) {
+                minValue = element;
             } else {
-                sortedQueue.add(element);
+                queue.add(element);
             }
         }
-        return sortedQueue;
+        queue.add(minValue);
     }
 
 
