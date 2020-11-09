8800456d7a4ba8156088e2fa066eb30c512733de
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-11-10 04:49:35 +0530

    adding methods to add and remove from window

--- algortihms-java/src/com/algo/window/MaxElementSubArray.java
+++ algortihms-java/src/com/algo/window/MaxElementSubArray.java
@@ -20,27 +20,30 @@ public class MaxElementSubArray {
         List<Integer> maxElements = new ArrayList<>();
         Deque<Integer> deque = new LinkedList<>();
         for (int i = 0; i < k; i++) {
-            while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[i]) {
-                deque.removeLast();
-            }
-            deque.addLast(i);
+            addToWindow(deque, arr, i);
         }
         for (int i = k; i < arr.length; i++) {
             maxElements.add(arr[deque.peek()]);
-
             // Remove the elements which
             // are out of this window
-            while (!deque.isEmpty() && deque.peek() <= i - k) {
-                deque.removeFirst();
-            }
-
-            while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[i]) {
-                deque.removeLast();
-            }
-            deque.addLast(i);
+            removeFromWindow(deque, k, i);
+            addToWindow(deque, arr, i);
         }
         //Loop doesn't add last element of window
         maxElements.add(arr[deque.peek()]);
         return maxElements;
+    }
+
+    private static void addToWindow(Deque<Integer> deque, int[] arr, int index) {
+        while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[index]) {
+            deque.removeLast();
+        }
+        deque.addLast(index);
+    }
+
+    private static void removeFromWindow(Deque<Integer> deque, int k, int index) {
+        while (!deque.isEmpty() && deque.peek() <= index - k) {
+            deque.removeFirst();
+        }
     }
 }
