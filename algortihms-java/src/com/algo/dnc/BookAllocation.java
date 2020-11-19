dba6a8f32ccaf61ff30b636df57fe2b26c9363ed
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-11-19 05:39:43 +0530

    Updating condition to calculate mid

--- algortihms-java/src/com/algo/dnc/BookAllocation.java
+++ algortihms-java/src/com/algo/dnc/BookAllocation.java
@@ -1,23 +1,22 @@
 package com.algo.dnc;
 
+import java.util.Arrays;
+
 public class BookAllocation {
 
     public static void main(String[] args) {
-        int[] books = {10, 20, 30, 40};
+        //int[] books = {10, 20, 30, 40};
+        int[] books = {12, 34, 67, 90};
         int totalStudents = 2;
         System.out.println("Minimum number of pages to read = " + getMinimumPages(books, totalStudents));
     }
 
     private static int getMinimumPages(int[] books, int totalStudents) {
-        int start = Integer.MIN_VALUE;
-        int totalPages = 0;
+        int start = books[books.length - 1];
+        int totalPages = Arrays.stream(books).sum();
         int pagesToRead = 0;
-        for (int book : books) {
-            totalPages += book;
-            start = Math.max(start, book);
-        }
         while (start <= totalPages) {
-            int mid = (start + totalPages) / 2;
+            int mid = start + (totalPages - start) / 2;
             if (validDistribution(books, totalStudents, mid)) {
                 pagesToRead = mid;
                 totalPages = mid - 1;
