75b51b909b428991e834cd87767c2c80a5a2213c
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-01-04 05:10:27 +0530

    Updating condition to change value of start and end

--- algortihms-java/src/com/algo/dnc/SearchRotated.java
+++ algortihms-java/src/com/algo/dnc/SearchRotated.java
@@ -2,7 +2,7 @@ package com.algo.dnc;
 
 public class SearchRotated {
     public static void main(String[] args) {
-        int[] arr = {5, 6, 7, 1, 2, 3, 4};
+        int[] arr = {5, 6, 7, 8, 9, 1, 2, 3, 4};
         int key = 2;
         int index = search(arr, key);
         if (index >= 0) {
@@ -16,29 +16,27 @@ public class SearchRotated {
     private static int search(int[] arr, int key) {
         int start = 0;
         int end = arr.length - 1;
-        int index = -1;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (arr[mid] == key) {
-                index = mid;
-                break;
+                return mid;
             }
             //First half is sorted
             if (arr[start] <= arr[mid]) {
-                if (key >= arr[start] && key <= arr[mid]) {
+                if (key >= arr[start] && key < arr[mid]) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             } else {
                 //Second half is sorted
-                if (key >= arr[mid] && key <= arr[end]) {
+                if (key > arr[mid] && key <= arr[end]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
                 }
             }
         }
-        return index;
+        return -1;
     }
 }
