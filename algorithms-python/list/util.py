380d0c1c068044e3be35c8d4e6c1686847eab637
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-02-20 06:44:24 +0530

    Adding method to print circular list

--- algorithms-python/list/util.py
+++ algorithms-python/list/util.py
@@ -8,3 +8,14 @@ def display(head: Node) -> None:
     while temp is not None:
         print(temp.data, end="\t")
         temp = temp.next
+
+
+def display_circular_list(head: Node) -> None:
+    if head is None:
+        return
+    temp: Node = head
+    while True:
+        print(temp.data, end="\t")
+        temp = temp.next
+        if temp == head:
+            break
