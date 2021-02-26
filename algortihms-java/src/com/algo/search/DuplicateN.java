63a4bbf0accded6447ee85d09451929c0c82dc69
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-02-27 05:14:21 +0530

    Check whether array has duplicates

--- algortihms-java/src/com/algo/search/DuplicateN.java
+++ algortihms-java/src/com/algo/search/DuplicateN.java
@@ -5,6 +5,7 @@ public class DuplicateN {
         int[] arr = {3, 2, 1, 2, 2, 3};
         //int[] arr = {1, 2, 3, 4, 5, 0};
         System.out.println("Array has duplicates = " + hasDuplicate(arr));
+        //  System.out.println("Array has duplicates = " + hasDuplicate1(arr));
     }
 
     private static boolean hasDuplicate(int[] arr) {
@@ -13,6 +14,18 @@ public class DuplicateN {
                 return true;
             }
             arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+        }
+        return false;
+    }
+
+    private static boolean hasDuplicate1(int[] arr) {
+        int[] indexes = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (indexes[arr[i]] == -1) {
+                return true;
+            } else {
+                indexes[arr[i]] = -1;
+            }
         }
         return false;
     }
