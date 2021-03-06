1678b08a084bfa4e4064baead7f05f7a03be8403
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-03-06 08:09:36 +0530

    Using hashset to find odd count

--- algortihms-java/src/com/algo/search/OddCount.java
+++ algortihms-java/src/com/algo/search/OddCount.java
@@ -1,12 +1,31 @@
 package com.algo.search;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OddCount {
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2};
-        System.out.println("Element with odd count = " + findOddCountElement(arr));
+        System.out.println("Element with odd count using hash table = " + findOddCountElementUsingHashset(arr));
+        System.out.println("Element with odd count using XOR operation = " + findOddCountElementUsingXOR(arr));
     }
 
-    private static int findOddCountElement(int[] arr) {
+    private static int findOddCountElementUsingHashset(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr) {
+            int count = map.getOrDefault(ele, 0);
+            ++count;
+            map.put(ele, count);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)%2!=-0){
+                return key;
+            }
+        }
+        return -1;
+    }
+
+    private static int findOddCountElementUsingXOR(int[] arr) {
         int result = 0;
         for (int i = 0; i < arr.length; i++) {
             result = result ^ arr[i];
