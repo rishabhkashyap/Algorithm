df4707a4f17453f7b7928a297cc21d7cab1b20d9
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-03-06 08:01:20 +0530

    Code refactoring

--- algortihms-java/src/com/algo/search/FirstDuplicate.java
+++ algortihms-java/src/com/algo/search/FirstDuplicate.java
@@ -5,42 +5,32 @@ import java.util.*;
 public class FirstDuplicate {
 
     public static void main(String[] args) {
-        int[] arr = {10, 5, 3, 4, 3, 5, 6};
+        int[] arr = {10, 5, 3, 4, 3, 5, 6, 7, 6};
         System.out.println("First repeated element in array using location = " + getFirstDuplicate(arr));
         System.out.println("First repeated element in array using hashing = " + getFirstDuplicateUsingHash(arr));
         System.out.println("First repeated element in array using hash set = " + getFirstDuplicateUsingHashset(arr));
 
     }
 
-    private static int getFirstDuplicateUsingHashset(int[] arr) {
-        Set<Integer> set = new HashSet<>();
-        int index = -1;
-        for (int i = arr.length - 1; i >= 0; --i) {
-            if (set.contains(arr[i])) {
-                index = i;
-            } else {
-                set.add(arr[i]);
-            }
-        }
-        return index != -1 ? arr[index] : -1;
-    }
-
     private static int getFirstDuplicate(int[] arr) {
         int[] temp = Arrays.copyOf(arr, arr.length);
         Arrays.sort(temp);
         for (int number : arr) {
-            if (isNumberRepeated(temp, number)) {
+            if (getFreq(temp, number) > 1) {
                 return number;
             }
         }
         return -1;
     }
 
-    private static boolean isNumberRepeated(int[] temp, int number) {
-        return getFirstOccurence(temp, number) != getLastOccurence(temp, number) ? true : false;
+
+    private static int getFreq(int[] arr, int element) {
+        int startIndex = getFirstOccurrence(arr, element);
+        int endIndex = getLastOccurrence(arr, element);
+        return endIndex - startIndex + 1;
     }
 
-    private static int getFirstOccurence(int[] arr, int key) {
+    private static int getFirstOccurrence(int[] arr, int key) {
         int start = 0;
         int end = arr.length - 1;
         int mid = 0;
@@ -59,7 +49,7 @@ public class FirstDuplicate {
         return pos;
     }
 
-    private static int getLastOccurence(int[] arr, int key) {
+    private static int getLastOccurrence(int[] arr, int key) {
         int start = 0;
         int end = arr.length - 1;
         int mid = 0;
@@ -79,24 +69,32 @@ public class FirstDuplicate {
     }
 
     private static int getFirstDuplicateUsingHash(int[] arr) {
-        int firstRepeated = -1;
         Map<Integer, Integer> freqMap = new LinkedHashMap<>();
         for (int number : arr) {
-            if (freqMap.containsKey(number)) {
-                int count = freqMap.get(number);
-                ++count;
-                freqMap.put(number, count);
-            } else {
-                freqMap.put(number, 1);
-            }
+            int count = freqMap.getOrDefault(number, 0);
+            ++count;
+            freqMap.put(number, count);
+
         }
         Set<Integer> keySet = freqMap.keySet();
         for (int number : keySet) {
             if (freqMap.get(number) > 1) {
-                firstRepeated = number;
-                break;
+                return number;
             }
         }
-        return firstRepeated;
+        return -1;
+    }
+
+    private static int getFirstDuplicateUsingHashset(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        int index = -1;
+        for (int i = arr.length - 1; i >= 0; --i) {
+            if (set.contains(arr[i])) {
+                index = i;
+            } else {
+                set.add(arr[i]);
+            }
+        }
+        return index != -1 ? arr[index] : -1;
     }
 }
