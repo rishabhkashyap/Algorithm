1f9dbaa5ece1c51515aaafd2fad2011de633ada9
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-04-18 15:12:19 +0530

    Rearranging methods and changing logic to encode column id to column name

--- algortihms-java/src/com/algo/strings/ExcelColumn.java
+++ algortihms-java/src/com/algo/strings/ExcelColumn.java
@@ -8,6 +8,20 @@ public class ExcelColumn {
         System.out.println("Decode column value = " + decodeColumn(column));
     }
 
+    private static String encodeColumn(int colId) {
+        StringBuilder sb = new StringBuilder();
+        while (colId != 0) {
+            int remainder = (colId - 1) % 26;
+            sb.append(getChar(remainder));
+            colId = (colId - 1) / 26;
+        }
+        return sb.reverse().toString();
+    }
+
+    private static char getChar(int i) {
+        return (char) (i + 'A');
+    }
+
     private static int decodeColumn(String column) {
         int result = 0;
         int multiplier = 1;
@@ -22,24 +36,5 @@ public class ExcelColumn {
 
     private static int getDigit(char character) {
         return character - 'A' + 1;
-    }
-
-    private static String encodeColumn(int colId) {
-        StringBuilder sb = new StringBuilder();
-        while (colId != 0) {
-            int remainder = colId % 26;
-            if (remainder == 0) {
-                sb.append('Z');
-                colId = colId / 26 - 1;
-            } else {
-                sb.append(getChar(remainder - 1));
-                colId /= 26;
-            }
-        }
-        return sb.reverse().toString();
-    }
-
-    private static char getChar(int i) {
-        return (char) (i + 'A');
     }
 }
