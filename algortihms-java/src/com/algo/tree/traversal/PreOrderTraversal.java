38ce16e6eea2310d9da4b3a8fd3453d81dea6c65
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-05-02 12:56:28 +0530

    Pre order traversal of binary tree

---
+++ algortihms-java/src/com/algo/tree/traversal/PreOrderTraversal.java
@@ -0,0 +1,48 @@
+package com.algo.tree.traversal;
+
+import com.algo.tree.Node;
+
+import java.util.Stack;
+
+public class PreOrderTraversal {
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        node1.setLeft(node2);
+        node1.setRight(node3);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node3.setLeft(node6);
+        //preOrder1(node1);
+        preOrder2(node1);
+
+    }
+
+    private static void preOrder1(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.getData() + "\t");
+        preOrder1(root.getLeft());
+        preOrder1(root.getRight());
+    }
+
+    private static void preOrder2(Node root) {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            System.out.print(node.getData() + "\t");
+            if (node.getRight() != null) {
+                stack.push(node.getRight());
+            }
+            if (node.getLeft() != null) {
+                stack.push(node.getLeft());
+            }
+        }
+    }
+}
