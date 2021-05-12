5c3bfe086df74ca8d6ce8dcd0f528899c2abc2a0
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-05-12 06:18:41 +0530

    Updating code to throw runtime exception when queue is empty

--- algortihms-java/src/com/algo/stack/QueueUsingOneStack.java
+++ algortihms-java/src/com/algo/stack/QueueUsingOneStack.java
@@ -3,31 +3,35 @@ package com.algo.stack;
 import java.util.Stack;
 
 public class QueueUsingOneStack {
-	private static Stack<Integer> stack = new Stack<>();
-	public static void main(String[] args){
-		eneque(1);
-		eneque(2);
-		stack.pop();
-		eneque(3);
-		eneque(4);
-		while(!stack.isEmpty()){
-			System.out.print(dequeue()+"\t");
-		}
-		
-	}
-	
-	private static void eneque(int n){
-		if(stack.isEmpty()){
-			stack.push(n);
-		}else{
-			int temp=stack.pop();
-			eneque(n);
-			stack.push(temp);
-		}
-	}
-	
-	private static int dequeue(){
-		return stack.pop();
-	}
+    private static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        eneque(1);
+        eneque(2);
+        stack.pop();
+        eneque(3);
+        eneque(4);
+        while (!stack.isEmpty()) {
+            System.out.print(dequeue() + "\t");
+        }
+
+    }
+
+    private static void eneque(int n) {
+        if (stack.isEmpty()) {
+            stack.push(n);
+        } else {
+            int temp = stack.pop();
+            eneque(n);
+            stack.push(temp);
+        }
+    }
+
+    private static int dequeue() {
+        if (stack.isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return stack.pop();
+    }
 
 }
