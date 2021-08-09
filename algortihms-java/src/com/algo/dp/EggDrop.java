52b6432c688f872a07ca5bf8ab403c1fa2423ab0
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-08-09 15:46:24 +0530

    Adding dp to problem

--- algortihms-java/src/com/algo/dp/EggDrop.java
+++ algortihms-java/src/com/algo/dp/EggDrop.java
@@ -1,5 +1,7 @@
 package com.algo.dp;
 
+import java.util.Arrays;
+
 public class EggDrop {
     public static void main(String[] args) {
         int eggs = 2;
@@ -8,21 +10,36 @@ public class EggDrop {
     }
 
     private static int findTrialCount(int eggs, int floors) {
+        int[][] dp = new int[eggs + 1][floors + 1];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        return findTrialCount(eggs, floors, dp);
+    }
+
+
+    private static int findTrialCount(int eggs, int floors, int[][] dp) {
         if (eggs == 0) {
             return 0;
         }
         if (floors == 0 || floors == 1) {
             return floors;
         }
+        //start from floor 1 then floor 2 and so on. Worst case scenario egg doesn't break
+        //till top floor therefore number of trial required is equal to number of floors
         if (eggs == 1) {
             return floors;
         }
+        if (dp[eggs][floors] != -1) {
+            return dp[eggs][floors];
+        }
         int minTrials = Integer.MAX_VALUE;
         for (int i = 1; i <= floors; i++) {
-            int result = Math.max(findTrialCount(eggs - 1, i - 1),
-                    findTrialCount(eggs, floors - i));
+            int result = Math.max(findTrialCount(eggs - 1, i - 1, dp),
+                    findTrialCount(eggs, floors - i, dp));
             minTrials = Math.min(minTrials, result);
         }
-        return minTrials + 1;
+        dp[eggs][floors] = minTrials + 1;
+        return dp[eggs][floors];
     }
 }
