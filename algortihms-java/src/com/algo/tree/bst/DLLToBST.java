b76b73587d7ea38dc6fb223cb409c8e4f52f6713
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-08-18 07:31:16 +0530

    Convert sorted dll to bst

--- algortihms-java/src/com/algo/tree/bst/DLLToBST.java
+++ algortihms-java/src/com/algo/tree/bst/DLLToBST.java
@@ -4,66 +4,14 @@ import com.algo.tree.Node;
 
 public class DLLToBST {
     public static void main(String[] args) {
-        Node head = null;
-        for (int i = 7; i >= 1; --i) {
-            if (head == null) {
-                head = new Node(i);
-            } else {
-                Node node = new Node(i);
-                node.setRight(head);
-                head.setLeft(node);
-                head = node;
-            }
-        }
-        // printList(head);
-        Node root = convertToBST(head);
+        DLL dll = new DLL();
+        dll.createdSortedDLL(7);
+        Node root = dll.convertToBST();
         traversal(root);
 
 
     }
 
-    private static Node convertToBST(Node head) {
-        int len = len(head);
-        //System.out.println(len);
-        return convertToBST(head, len);
-    }
-
-    private static Node convertToBST(Node head, int len) {
-        if (len <= 0) {
-            return null;
-        }
-        Node left = convertToBST(head, len / 2);
-        Node root = head;
-        head = head.getLeft();
-        root.setLeft(left);
-        root.setRight(convertToBST(head, len - len / 2 - 1));
-        return root;
-    }
-
-    private static int len(Node head) {
-        int count = 0;
-        while (head != null) {
-            ++count;
-            head = head.getRight();
-        }
-        return count;
-    }
-
-    private static void printList(Node head) {
-        Node temp = head;
-        Node tail = null;
-        System.out.println("Traversing from head");
-        while (temp != null) {
-            tail = temp;
-            System.out.print(temp.getData() + "\t");
-            temp = temp.getRight();
-        }
-        System.out.println("\nTraversing from tail");
-        while (tail != null) {
-            System.out.print(tail.getData() + "\t");
-            tail = tail.getLeft();
-        }
-    }
 
     private static void traversal(Node root) {
         if (root == null) {
@@ -72,5 +20,77 @@ public class DLLToBST {
         traversal(root.getLeft());
         System.out.print(root.getData() + "\t");
         traversal(root.getLeft());
+    }
+
+    private static class DLL {
+        private Node head;
+
+        public Node createdSortedDLL(int n) {
+            for (int i = n; i >= 1; --i) {
+                Node node = new Node(i);
+                insertArFront(node);
+            }
+
+            return head;
+
+        }
+
+        private void insertArFront(Node node) {
+            if (head == null) {
+                this.head = node;
+            } else {
+
+                node.setRight(head);
+                head.setLeft(node);
+                head = node;
+            }
+
+
+        }
+
+        public void printList() {
+            Node temp = head;
+            Node tail = null;
+            System.out.println("Traversing from head");
+            while (temp != null) {
+                tail = temp;
+                System.out.print(temp.getData() + "\t");
+                temp = temp.getRight();
+            }
+            System.out.println("\nTraversing from tail");
+            while (tail != null) {
+                System.out.print(tail.getData() + "\t");
+                tail = tail.getLeft();
+            }
+        }
+
+        public Node convertToBST() {
+            int len = len();
+            return convertToBST(len);
+        }
+
+        private int len() {
+            Node temp=head;
+            int count = 0;
+            while (temp != null) {
+                ++count;
+                temp = temp.getRight();
+            }
+            return count;
+        }
+
+        private Node convertToBST(int len) {
+            if (len <= 0) {
+                return null;
+            }
+            Node left = convertToBST(len / 2);
+            Node root = head;
+            root.setLeft(left);
+            head = head.getRight();
+            root.setRight(convertToBST(len - len / 2 - 1));
+            return root;
+        }
+
+
     }
 }
