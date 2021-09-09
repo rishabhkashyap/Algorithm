89d8862d334cea483152ef5cd07ba68c7c81b8a0
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-09-09 07:00:38 +0530

    Updating code to handle edge cases in interative approach

--- algortihms-java/src/com/algo/heaps/HeapCheck.java
+++ algortihms-java/src/com/algo/heaps/HeapCheck.java
@@ -4,17 +4,17 @@ public class HeapCheck {
     public static void main(String[] args) {
         //int[] heap = {10, 9, 6, 2, 1};
         int[] heap = {90, 15, 10, 7, 12, 2};
-        //int[] heap={1,2,3,4,5,6};
-        //boolean result = isMaxHeapRecursive(heap);
-        boolean result = isMaxHeapRecursive(heap);
-        System.out.println("heap is max heap = " + result);
+        int[] heap1={1,2,3,4,5,6};
+        System.out.println("heap is max heap (iterative)= " + isMaxHeapIterative(heap));
+        System.out.println("heap is max heap(recursive) = " + isMaxHeapRecursive(heap));
     }
 
     private static boolean isMaxHeapRecursive(int[] heap) {
-        return isMaxHeapRecursiveHelper(heap, heap.length-1, 0);
+        return isMaxHeapRecursiveHelper(heap, heap.length - 1, 0);
     }
 
     private static boolean isMaxHeapRecursiveHelper(int[] heap, int size, int i) {
+        // if `i` is a leaf node, return true as every leaf node is a heap
         if (2 * i + 2 > size) {
             return true;
         }
@@ -24,22 +24,23 @@ public class HeapCheck {
     }
 
     private static boolean isMaxHeapIterative(int[] heap) {
-        boolean isMaxHeap = true;
         int size = heap.length;
-        for (int i = 0; i < heap.length / 2; i++) {
+        for (int i = 0; i <= (heap.length-2) / 2; i++) {
             int rightIndex = 2 * i + 1;
             int leftIndex = 2 * i + 2;
-            if (rightIndex < size && leftIndex < size && (heap[i] < heap[rightIndex]
-                    || heap[i] < heap[rightIndex])) {
-                isMaxHeap = false;
-                break;
+//            if (rightIndex >= size || leftIndex >= size) {
+//                return false;
+//            }
+            if ((rightIndex <size && leftIndex < size) &&
+                    (heap[i] < heap[leftIndex] || heap[i] < heap[rightIndex])) {
+                return false;
             }
         }
-        return isMaxHeap;
+        return true;
     }
 
     private static boolean isMinHeapRecursive(int[] heap) {
-        return isMinHeapRecursiveHelper(heap, heap.length-1, 0);
+        return isMinHeapRecursiveHelper(heap, heap.length - 1, 0);
     }
 
     private static boolean isMinHeapRecursiveHelper(int[] heap, int size, int i) {
