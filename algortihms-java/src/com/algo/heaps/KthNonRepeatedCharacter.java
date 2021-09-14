8d4440173d065844358603b6aae19c459e12378b
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-09-14 07:18:58 +0530

    Updating file name

--- algortihms-java/src/com/algo/heaps/KthNonRepeatedCharacter.java
+++ algortihms-java/src/com/algo/heaps/KthNonRepeatedCharacter.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-public class KNonRepeatedCharacter {
+public class KthNonRepeatedCharacter {
 
     public static void main(String[] args) {
         String str = "abcbcdeabf";
