0dc43d046601f6ef808244a12aa513af763d1bcf
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-09-28 07:02:14 +0530

    Fixing typos and rearranging methods

--- algortihms-java/src/com/algo/heaps/Medain.java
+++ algortihms-java/src/com/algo/heaps/Medain.java
@@ -8,31 +8,26 @@ public class Medain {
 
     public static void main(String[] args) {
         int[] stream = {5, 15, 1, 3, 2, 8, 7, 9, 10, 6, 11, 4};
-        printMedain(stream);
+        printMedian(stream);
     }
 
-    private static void printMedain(int[] stream) {
+    private static void printMedian(int[] stream) {
         Queue<Integer> lower = new PriorityQueue<>(Collections.reverseOrder());
         Queue<Integer> higher = new PriorityQueue<>();
         for (int number : stream) {
             addElement(number, lower, higher);
             balanceHeaps(lower, higher);
-            double medain = getMedain(lower, higher);
-            System.out.println("Medain = " + medain);
+            double medain = getMedian(lower, higher);
+            System.out.println("Median = " + medain);
         }
     }
 
-    private static double getMedain(Queue<Integer> lower, Queue<Integer> higher) {
-
-        double medain = 0;
-        if (lower.size() == higher.size()) {
-            medain = (double) (lower.peek() + higher.peek()) / 2;
+    private static void addElement(int number, Queue<Integer> lower, Queue<Integer> higher) {
+        if (lower.size() == 0 || number < lower.peek()) {
+            lower.add(number);
         } else {
-            Queue<Integer> bigHeap = Math.max(lower.size(), higher.size()) == lower.size() ? lower : higher;
-            medain = bigHeap.peek();
+            higher.add(number);
         }
-        return medain;
-
     }
 
     private static void balanceHeaps(Queue<Integer> lower, Queue<Integer> higher) {
@@ -44,11 +39,16 @@ public class Medain {
 
     }
 
-    private static void addElement(int number, Queue<Integer> lower, Queue<Integer> higher) {
-        if (lower.size() == 0 || number < lower.peek()) {
-            lower.add(number);
+    private static double getMedian(Queue<Integer> lower, Queue<Integer> higher) {
+
+        double medain = 0;
+        if (lower.size() == higher.size()) {
+            medain = (double) (lower.peek() + higher.peek()) / 2;
         } else {
-            higher.add(number);
+            Queue<Integer> bigHeap = Math.max(lower.size(), higher.size()) == lower.size() ? lower : higher;
+            medain = bigHeap.peek();
         }
+        return medain;
+
     }
 }
