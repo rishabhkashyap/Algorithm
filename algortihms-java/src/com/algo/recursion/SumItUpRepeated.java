807c77e5bb3efe3d58ef3a60f18ad7ae32d7e3d7
rishabhkashyap <kashyap.rishab@hotmail.com>
2022-01-29 08:02:25 +0530

    Code refactoring

--- algortihms-java/src/com/algo/recursion/SumItUpRepeated.java
+++ algortihms-java/src/com/algo/recursion/SumItUpRepeated.java
@@ -6,72 +6,46 @@ import java.util.List;
 
 //Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C
 //where the candidate numbers sums to T. The same repeated number may be chosen from C unlimited number of times.
-
+//Problem: https://leetcode.com/problems/combination-sum/
 public class SumItUpRepeated {
 
     public static void main(String[] args) throws IOException {
 
         int[] input = {2, 3, 5, 6, 8};
         int target = 10;
-        printSumCombination(input, target);
-        System.out.println("********************************************");
-        printSumCombination2(input, target);
-    }
-
-    private static void printSumCombination(int[] input, int target) {
-        List<Integer> sumNumber = new ArrayList<>();
-        printSumCombinationHelper(input, target, sumNumber, 0);
-    }
-
-    private static boolean printSumCombinationHelper(int[] input, int target, List<Integer> sumNumber, int start) {
-        if (target == 0) {
-            printList(sumNumber);
-            return false;
-        }
-        if (target < 0) {
-            return false;
-        }
-
-        for (int j = start; j < input.length; j++) {
-
-            sumNumber.add(input[j]);
-            boolean sumFound = printSumCombinationHelper(input, target - input[j], sumNumber, j);
-            if (sumFound) {
-                return true;
-            }
-            sumNumber.remove(sumNumber.size() - 1);
-
-
-        }
-        return false;
+        List<List<Integer>> result=combinationSum(input, target);
+        result.forEach(e->System.out.println(e));
     }
 
 
-    private static void printSumCombination2(int[] input, int target) {
-        List<Integer> sumNumber = new ArrayList<>();
-        printSumCombination2(input, target, sumNumber, 0);
-    }
 
-    private static void printSumCombination2(int[] input, int target, List<Integer> sumNumber, int start) {
-        if (target == 0) {
-            printList(sumNumber);
-            return;
-        }
-        if (target < 0) {
-            return;
-        }
+	public static List<List<Integer>> combinationSum(int[] arr, int target) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> combination = new ArrayList<>();
+		combinationSum(arr, target, combination, result, 0, 0);
+		return result;
+	}
 
-        for (int j = start; j < input.length; j++) {
-            sumNumber.add(input[j]);
-            printSumCombinationHelper(input, target - input[j], sumNumber, j);
-            sumNumber.remove(sumNumber.size() - 1);
-        }
-    }
+	public static void combinationSum(int[] arr, int target, List<Integer> combination,
+			List<List<Integer>> result, int i, int sum) {
+		if (i > arr.length) {
+			return;
+		}
+		if (sum == target) {
+			result.add(new ArrayList<>(combination));
+			return;
+		}
+		if (sum > target) {
+			return;
+		}
 
+		for (int j = i; j < arr.length; j++) {
 
-    private static void printList(List<Integer> sumNumber) {
-        sumNumber.forEach(e -> System.out.print(e + " "));
-        System.out.println();
-    }
+			combination.add(arr[j]);
+			combinationSum(arr, target, combination, result, j, sum + arr[j]);
+			combination.remove(combination.size() - 1);
+
+		}
+	}
 
 }
