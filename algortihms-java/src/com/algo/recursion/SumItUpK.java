a39e107b2fd63e4422b33f38885ead30d356f94d
rishabhkashyap <kashyap.rishab@hotmail.com>
2022-01-29 08:35:48 +0530

    Code refactoring

--- algortihms-java/src/com/algo/recursion/SumItUpK.java
+++ algortihms-java/src/com/algo/recursion/SumItUpK.java
@@ -2,43 +2,44 @@ package com.algo.recursion;
 
 
 //Find all possible combinations of k numbers that add up to a number n,
-//given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+//given that only numbers from 1 to 9 can be used and each combination should
+// be a unique set of numbers.
+//Problem: https://leetcode.com/problems/combination-sum-iii/
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SumItUpK {
 
-    public static void main(String[] args) {
-        int target = 10;
-        int k = 3;
-        printCompinations(target, k);
+	public static void main(String[] args) {
+		int target = 10;
+		int k = 3;
+		List<List<Integer>> result = combinationSum3(k, target);
+		result.forEach(e -> System.out.println(e));
 
-    }
+	}
 
-    private static void printCompinations(int target, int k) {
-        List<Integer> numbers = new ArrayList<>();
-        printCombinationHelper(target, k, numbers, 1);
-    }
+	public static List<List<Integer>> combinationSum3(int k, int n) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> nums = new ArrayList<>();
+		combinationSum3(nums, 1, k, n, result);
+		return result;
 
-    private static void printCombinationHelper(int target, int k, List<Integer> numbers, int i) {
-        if (target == 0 && numbers.size() == k) {
-            printList(numbers);
-            return;
-        }
+	}
 
+	private static void combinationSum3(List<Integer> nums, int i, int k, int n, List<List<Integer>> result) {
+		if (nums.size() == k && n == 0) {
+			result.add(new ArrayList<>(nums));
+			return;
+		}
+		if (i > 9 || n < 0 || nums.size() > k) {
+			return;
+		}
 
-        if (target < 0) {
-            return;
-        }
-        for (int j = i; j <= 9; j++) {
-            numbers.add(j);
-            printCombinationHelper(target - j, k, numbers, j + 1);
-            numbers.remove(numbers.size() - 1);
-        }
-    }
-
-    private static void printList(List<Integer> sumNumber) {
-        sumNumber.forEach(e -> System.out.print(e + " "));
-        System.out.println();
-    }
+		for (int j = i; j <= 9; j++) {
+			nums.add(j);
+			combinationSum3(nums, j + 1, k, n - j, result);
+			nums.remove(nums.size() - 1);
+		}
+	}
 }
