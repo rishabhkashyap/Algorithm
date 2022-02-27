06f227a4698506fffec33d98d0766ca3b408f685
rishabhkashyap <kashyap.rishab@hotmail.com>
2022-02-27 12:10:07 +0530

    Alternate way to fill table and compute longest common subsequence

--- algortihms-java/src/com/algo/dp/LongestCommonSubSequence.java
+++ algortihms-java/src/com/algo/dp/LongestCommonSubSequence.java
@@ -1,8 +1,6 @@
 package com.algo.dp;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class LongestCommonSubSequence {
@@ -14,6 +12,7 @@ public class LongestCommonSubSequence {
         System.out.println(getLongestSubsequenceLength1(text1, text2));
         System.out.println(getLongestSubsequenceLength2(text1, text2));
         System.out.println(getLongestSubsequenceLength3(text1, text2));
+        System.out.println(getLongestSubsequenceLength4(text1, text2));
 
     }
 
@@ -68,17 +67,29 @@ public class LongestCommonSubSequence {
         for (int i = text1.length() - 1; i >= 0; --i) {
             for (int j = text2.length() - 1; j >= 0; --j) {
                 if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i][j] = ++dp[i+1][j+1];
+                    dp[i][j] = ++dp[i + 1][j + 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
                 }
             }
         }
         return dp[0][0];
     }
 
+    private static int getLongestSubsequenceLength4(String str1, String str2) {
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = ++dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
 
-
+            }
+        }
+        return dp[str1.length()][str2.length()];
+    }
 
 
 }
