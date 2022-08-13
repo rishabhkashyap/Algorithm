168a3c6a64553ceebaa5e00fc786fb787aace78d
rishabh <rishabh.kashyap@oracle.com>
2022-08-13 18:28:05 +0530

    Updating code for edge cases

--- algortihms-java/src/com/algo/dnc/BinarySearchRotatedLargestElement.java
+++ algortihms-java/src/com/algo/dnc/BinarySearchRotatedLargestElement.java
@@ -1,8 +1,10 @@
 package com.algo.dnc;
 
+//Problem: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 public class BinarySearchRotatedLargestElement {
     public static void main(String[] args) {
         int[] arr = {5, 6, 1, 2, 3, 4};
+        //int[] arr = {11, 13, 15, 17};
         System.out.println("Largest element in rotated array = " + getLargest(arr));
         System.out.println("Smallest element in rotated array = " + getSmallest(arr));
     }
@@ -10,15 +12,15 @@ public class BinarySearchRotatedLargestElement {
     private static int getLargest(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-        int pivot = -99999;
+        if (arr[start] <= arr[end]) {
+            return arr[end];
+        }
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] < arr[mid - 1]) {
-                pivot = arr[mid - 1];
-                break;
-            } else if (arr[mid] > arr[mid + 1]) {
-                pivot = arr[mid];
-                break;
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return arr[mid - 1];
+            } else if (mid < end && arr[mid] > arr[mid + 1]) {
+                return arr[mid];
             } else if (arr[start] >= arr[mid]) {
                 //Left part of array is unsorted
                 end = mid - 1;
@@ -27,29 +29,31 @@ public class BinarySearchRotatedLargestElement {
                 start = mid + 1;
             }
         }
-        return pivot;
+        return Integer.MIN_VALUE;
     }
 
     private static int getSmallest(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-        int pivot = -99999;
+        //Array is sorted
+        if (arr[start] <= arr[end]) {
+            return arr[start];
+        }
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] < arr[mid - 1]) {
-                pivot = arr[mid];
-                break;
-            } else if (arr[mid] > arr[mid + 1]) {
-                pivot = arr[mid + 1];
-                break;
-            } else if (arr[start] >= arr[mid]) {
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return arr[mid];
+            } else if (mid < end && arr[mid] > arr[mid + 1]) {
+                return arr[mid + 1];
+
+            } else if (arr[start] <= arr[mid]) {
                 //Left part of array is unsorted
-                end = mid - 1;
-            } else if (arr[end] <= arr[mid]) {
-                //Right part of array is not sorted
                 start = mid + 1;
+            } else {
+                //Right part of array is not sorted
+                end = mid - 1;
             }
         }
-        return pivot;
+        return -1;
     }
 }
