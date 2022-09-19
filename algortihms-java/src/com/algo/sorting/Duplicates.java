a2a2aac7f04ba1e1c2be777c71b8e7dd673d02ad
rishabh kashyap <kashyap.rishab@hotmail.com>
2022-09-19 07:12:32 +0530

    Updating code

--- algortihms-java/src/com/algo/sorting/Duplicates.java
+++ algortihms-java/src/com/algo/sorting/Duplicates.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Duplicates {
     public static void main(String[] args) {
-         int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+        int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
 //        int[] arr = {2, 2, 2, 2, 2};
         removeDuplicates(arr);
     }
@@ -13,10 +13,11 @@ public class Duplicates {
         int index = 0;
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] != arr[i + 1]) {
+                arr[index] = arr[i];
                 ++index;
-                arr[index] = arr[i + 1];
             }
         }
+        arr[index] = arr[arr.length - 1];
         Arrays.fill(arr, index + 1, arr.length, 0);
         Arrays.stream(arr).forEach(e -> System.out.print(e + "\t"));
     }
