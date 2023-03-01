a4816b119012206ba6f84cd8c98cb5e5fe66e21c
rishabh kashyap <kashyap.rishab@hotmail.com>
2023-03-01 07:08:46 +0530

    Updating code to pass edge cases

--- algortihms-java/src/com/algo/tree/InPreTree.java
+++ algortihms-java/src/com/algo/tree/InPreTree.java
@@ -1,5 +1,6 @@
 package com.algo.tree;
 
+//Problem: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 public class InPreTree {
 
     public static void main(String[] args) {
@@ -10,31 +11,31 @@ public class InPreTree {
     }
 
     private static Node constructTree(int[] inOrder, int[] preOrder) {
-        Index index = new Index();
-        Node root = constructTreeHelper(inOrder, preOrder, 0, inOrder.length - 1, index);
-        return root;
+        Position rootPos = new Position(0);
+        return constructTree(preOrder, rootPos, inOrder, 0, inOrder.length - 1);
     }
 
-    private static Node constructTreeHelper(int[] inOrder, int[] preOrder, int start, int end, Index preIndex) {
-        if (start > end) {
+    private static Node constructTree(int[] pre, Position rootPos, int[] in, int start, int end) {
+        if (rootPos.index >= pre.length) {
             return null;
         }
-        Node root = new Node(preOrder[preIndex.i++]);
+        Node node = new Node(pre[rootPos.index++]);
         if (start == end) {
-            return root;
+            return node;
         }
-        int inIndex = search(inOrder, root.getData(), start, end);
-        if (inIndex >= 0) {
-            root.setLeft(constructTreeHelper(inOrder, preOrder, start, inIndex - 1, preIndex));
-            root.setRight(constructTreeHelper(inOrder, preOrder, inIndex + 1, end, preIndex));
+        int loc = find(in, node.getData(), start, end);
+        if (loc > start) {
+            node.setLeft(constructTree(pre, rootPos, in, start, loc - 1));
         }
-        return root;
-
+        if (end > loc) {
+            node.setRight(constructTree(pre, rootPos, in, loc + 1, end));
+        }
+        return node;
     }
 
-    private static int search(int[] inOrder, int key, int start, int end) {
+    private static int find(int[] arr, int key, int start, int end) {
         for (int i = start; i <= end; i++) {
-            if (inOrder[i] == key) {
+            if (arr[i] == key) {
                 return i;
             }
         }
@@ -49,8 +50,12 @@ public class InPreTree {
         }
     }
 
-    private static class Index {
-        private int i;
+    private static class Position {
+        private int index;
+
+        private Position(int value) {
+            this.index = value;
+        }
     }
 
 
