94f34d54a9e589ab59250c33d12ab173924026cf
rishabh kashyap <kashyap.rishab@hotmail.com>
2023-07-17 05:11:56 +0530

    Leetcode 19: Remove Nth Node From End of List

---
+++ algortihms-java/src/com/algo/linkedList/NthNode.java
@@ -0,0 +1,60 @@
+package com.algo.linkedList;
+
+//Problem: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+public class NthNode {
+    public static void main(String[] args) {
+        List list = new List();
+        int n = 5;
+        for (int i = 1; i <= 5; i++) {
+            list.addLast(new Node(i));
+        }
+        System.out.println("Nth node from last = "+getNthFromLast(list.getHead(), n));
+        Node node = removeNthNodeEnd(list.getHead(), n);
+        list.display(node);
+    }
+
+    private static int getNthFromLast(Node head, int n) {
+        int count = 0;
+        Node temp = head;
+        Node current = head;
+        while (count < n - 1) {
+            current = current.next;
+            ++count;
+            if (current == null) {
+                return -1;
+            }
+        }
+        while (current != null && current.next != null) {
+            if (temp == null) {
+                temp = head;
+            } else {
+                temp = temp.next;
+                current = current.next;
+            }
+        }
+        assert temp != null;
+        return temp.getValue();
+    }
+
+    private static Node removeNthNodeEnd(Node head, int n) {
+        int count = 0;
+        Node temp = head;
+        Node current = head;
+        //Stop temp at n-1th node
+        while (count < n) {
+            current = current.getNext();
+            ++count;
+            if (current == null) {
+                return head.next;
+            }
+        }
+        while (current != null && current.getNext() != null) {
+            current = current.next;
+            temp = temp.next;
+        }
+        assert temp != null;
+        temp.next = temp.next.next;
+        return head;
+    }
+
+}
