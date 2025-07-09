3e21f399ff2280b36df20a43dc47baea607f26c3
rishabh kashyap <kashyap.rishab@hotmail.com>
2025-07-09 09:41:35 +0530

    Leetcode257: Adding improved solution

--- algortihms-java/src/com/algo/tree/BinaryTreePath257.java
+++ algortihms-java/src/com/algo/tree/BinaryTreePath257.java
@@ -1,6 +1,7 @@
 package com.algo.tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -17,9 +18,6 @@ public class BinaryTreePath257 {
         List<String> result = getPaths(root);
         result.forEach(e -> System.out.print(e + "\t"));
         result = getPaths2(root);
-        System.out.println();
-        result.forEach(e -> System.out.print(e + "\t"));
-        result = getPaths3(root);
         System.out.println();
         result.forEach(e -> System.out.print(e + "\t"));
     }
@@ -46,68 +44,44 @@ public class BinaryTreePath257 {
 
     //Iterative solution
     private static List<String> getPaths2(TreeNode root) {
-        Stack<Pair> stack = new Stack<>();
-        stack.push(new Pair(root, ""));
-        List<String> result = new ArrayList<>();
-        while (!stack.isEmpty()) {
-            Pair pair = stack.pop();
-            pair.path = pair.path + pair.node.val;
-            if (pair.node.right == null && pair.node.left == null) {
-                result.add(pair.path);
-            }
-            if (pair.node.left != null) {
-                stack.push(new Pair(pair.node.left, pair.path + "->"));
-            }
-            if (pair.node.right != null) {
-                stack.push(new Pair(pair.node.right, pair.path + "->"));
-            }
-        }
-        return result;
-    }
-
-    private static List<String> getPaths3(TreeNode root) {
-        List<String> result = new ArrayList<>();
-        Pair pair = new Pair(root, "");
-        getPaths3(root, pair, result);
-        return result;
-    }
-
-    private static void getPaths3(TreeNode root, Pair pair, List<String> result) {
         if (root == null) {
-            return;
+            return Collections.emptyList();
         }
-        pair.path = pair.path + root.val;
-        getPaths3(root.left, new Pair(root.left, pair.path + "->"), result);
-        getPaths3(root.right, new Pair(root.right, pair.path + "->"), result);
-        if (root.left == null && root.right == null) {
-            result.add(pair.path);
+        List<String> result = new ArrayList<>();
+        Stack<Pair> stack = new Stack<>();
+        var sb = new StringBuilder();
+        sb.append(root.val);
+        stack.push(new Pair(root, sb));
+        while (!stack.isEmpty()) {
+            var pair = stack.pop();
+            var node = pair.node;
+            var path = pair.path;
+            if (node.left == null && node.right == null) {
+                result.add(path.toString());
+            } else {
+                if (node.right != null) {
+                    sb = new StringBuilder(path);
+                    sb.append("->");
+                    sb.append(node.right.val);
+                    var rightPair = new Pair(node.right, sb);
+                    stack.push(rightPair);
+                }
+                if (node.left != null) {
+                    sb = new StringBuilder(path);
+                    sb.append("->");
+                    sb.append(node.left.val);
+                    var leftPair = new Pair(node.left, sb);
+                    stack.push(leftPair);
+                }
+            }
         }
+        return result;
     }
 
-    private static class Pair {
-        TreeNode node;
-        String path;
-
-        public Pair(TreeNode node, String path) {
-            this.node = node;
-            this.path = path;
-        }
-
-        public TreeNode getNode() {
-            return node;
-        }
-
-        public void setNode(TreeNode node) {
-            this.node = node;
-        }
-
-        public String getPath() {
-            return path;
-        }
-
-        public void setPath(String path) {
-            this.path = path;
-        }
+    private record Pair(
+            TreeNode node,
+            StringBuilder path
+    ) {
     }
 
     private static class TreeNode {
