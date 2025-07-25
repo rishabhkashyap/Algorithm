33c5ee19f5227137b3ca1819d9b72632ff58cab0
rishabh kashyap <kashyap.rishab@hotmail.com>
2025-07-25 10:28:11 +0530

    Leetcode 77: Adding solution without loop

--- algortihms-java/src/com/algo/recursion/Combinations77.java
+++ algortihms-java/src/com/algo/recursion/Combinations77.java
@@ -10,6 +10,7 @@ public class Combinations77 {
         int k = 2;
         List<List<Integer>> result = getCombinations(n, k);
         System.out.println(result);
+        System.out.println(getCombinations2(n, k));
     }
 
     private static List<List<Integer>> getCombinations(int n, int k) {
@@ -29,5 +30,26 @@ public class Combinations77 {
             helper(n, j + 1, k, list, result);
             list.remove(list.size() - 1);
         }
+    }
+
+    private static List<List<Integer>> getCombinations2(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        createCombinations(n, k, list, result);
+        return result;
+    }
+
+    private static void createCombinations(int n, int k, List<Integer> list, List<List<Integer>> result) {
+        if (n < 0) {
+            return;
+        }
+        if (list.size() == k) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        list.add(n);
+        createCombinations(n - 1, k, list, result);
+        list.remove(list.size() - 1);
+        createCombinations(n - 1, k, list, result);
     }
 }
