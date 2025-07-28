bf60cf67ce58df9395b4fc9b8486f1d8e98e31d6
rishabh kashyap <kashyap.rishab@hotmail.com>
2025-07-28 09:44:19 +0530

    Leetcode 40: Adding better solution

--- algortihms-java/src/com/algo/recursion/SumItUpUnique.java
+++ algortihms-java/src/com/algo/recursion/SumItUpUnique.java
@@ -1,55 +1,43 @@
 package com.algo.recursion;
-
-
 //Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C
 //where the candidate numbers sums to T. Each number in C may only be used ONCE in the combination.
 //Problem: https://leetcode.com/problems/combination-sum-ii/
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
 public class SumItUpUnique {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        int[] arr = {10, 1, 2, 7, 6, 1, 5};
+        int target = 8;
+        System.out.println(getCombinations(arr, target));
+    }
 
+    private static List<List<Integer>> getCombinations(int[] arr, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(arr);
+        helper(arr, 0, target, list, result);
+        return result;
+    }
 
-		int[] arr = { 10, 1, 2, 7, 6, 1, 5 };
-		int target = 8;
-		System.out.println(combinationSum(arr, target));
-	}
-
-	public static List<List<Integer>> combinationSum(int[] arr, int target) {
-		List<List<Integer>> result = new ArrayList<>();
-		List<Integer> combination = new ArrayList<>();
-		Arrays.sort(arr);
-		combinationSum(arr, target, combination, result, 0, 0);
-		return result;
-	}
-
-	public static void combinationSum(int[] arr, int target, List<Integer> combination,
-			List<List<Integer>> result, int i, int sum) {
-		if (sum == target) {
-			result.add(new ArrayList<>(combination));
-			return;
-		}
-		if (i >= arr.length) {
-			return;
-		}
-		if (sum > target) {
-			return;
-		}
-
-		for (int j = i; j < arr.length; j++) {
-			if (j > i && arr[j - 1] == arr[j]) {
-				continue;
-			}
-			combination.add(arr[j]);
-			combinationSum(arr, target, combination, result, j + 1, sum + arr[j]);
-			combination.remove(combination.size() - 1);
-
-		}
-	}
+    private static void helper(int[] arr, int i, int target, List<Integer> list, List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        if (target < 0 || i >= arr.length) {
+            return;
+        }
+        list.add(arr[i]);
+        helper(arr, i + 1, target - arr[i], list, result);
+        list.remove(list.size() - 1);
+        while (i + 1 < arr.length && arr[i] == arr[i + 1]) {
+            ++i;
+        }
+        helper(arr, i + 1, target, list, result);
+    }
 
 }
