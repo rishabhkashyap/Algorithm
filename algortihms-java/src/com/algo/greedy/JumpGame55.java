d7c3923a4f5b3c1370e7c202b526e2f3ca4df1ef
rishabh kashyap <kashyap.rishab@hotmail.com>
2026-01-16 08:46:20 +0530

    Leetcode55: Jump game

--- algortihms-java/src/com/algo/greedy/JumpGame55.java
+++ algortihms-java/src/com/algo/greedy/JumpGame55.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 
 public class JumpGame55 {
     public static void main(String[] args) {
-        int[] arr = {3, 2, 1, 0, 4};
-        //int[] arr = {2, 3, 1, 1, 4};
+        //int[] arr = {3, 2, 1, 0, 4};
+        int[] arr = {2, 3, 1, 1, 4};
         System.out.println(canReachEnd1(arr));
         System.out.println(canReachEnd2(arr));
     }
@@ -25,15 +25,15 @@ public class JumpGame55 {
         if (dp[i] != -1) {
             return dp[i] == 1;
         }
+        dp[i] = 0;
         for (int j = 1; j <= arr[i]; j++) {
             boolean result = canReachEnd1(arr, j + i, dp);
             if (result) {
                 dp[i] = 1;
-                return true;
+                break;
             }
         }
-        dp[i] = 2;
-        return false;
+        return dp[i] == 1;
     }
 
     //Greedy solution
