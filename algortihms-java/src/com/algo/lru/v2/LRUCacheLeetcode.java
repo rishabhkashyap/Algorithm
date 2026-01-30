4a0c339903603b6dd4c9144e35b1cb67eade3aa9
rishabh kashyap <kashyap.rishab@hotmail.com>
2026-01-30 08:41:32 +0530

    Leetcode146: Updated code

--- algortihms-java/src/com/algo/lru/v2/LRUCacheLeetcode.java
+++ algortihms-java/src/com/algo/lru/v2/LRUCacheLeetcode.java
@@ -17,53 +17,56 @@ public class LRUCacheLeetcode {
     }
 
     private static class LRUCache {
-        Node head;
-        Node tail;
-        private Map<Integer, Node> map;
-        private int capacity;
-        private int size;
+
+        private final Map<Integer, Node> map;
+
+        private final Integer capacity;
+
+        private final Node head;
+
+        private final Node tail;
 
         public LRUCache(int capacity) {
             this.map = new HashMap<>();
-            this.size = 0;
             this.capacity = capacity;
-            this.head = new Node(-1, -1);
-            this.tail = new Node(-1, -1);
+            head = new Node(-1, -1);
+            tail = new Node(-1, -1);
             head.next = tail;
             tail.prev = head;
         }
 
         public void put(int key, int value) {
             if (map.containsKey(key)) {
-                Node node = map.get(key);
+                var node = map.get(key);
                 node.value = value;
                 remove(node);
                 addFirst(node);
-
             } else {
-
-                Node node = new Node(key, value);
-                if (size < capacity) {
-                    map.put(key, node);
-                    addFirst(node);
-                    ++size;
-                } else {
+                if (map.size() >= capacity) {
                     map.remove(tail.prev.key);
                     remove(tail.prev);
-                    map.put(key, node);
-                    addFirst(node);
-
                 }
+                var node = new Node(key, value);
+                map.put(key, node);
+                addFirst(node);
             }
         }
 
         public int get(int key) {
             if (map.containsKey(key)) {
-                remove(map.get(key));
-                addFirst(map.get(key));
-                return map.get(key).value;
+                var node = map.get(key);
+                remove(node);
+                addFirst(node);
+                return node.value;
             }
             return -1;
+        }
+
+        public void remove(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            node.next = null;
+            node.prev = null;
         }
 
         public void addFirst(Node node) {
@@ -73,25 +76,19 @@ public class LRUCacheLeetcode {
             node.next.prev = node;
         }
 
-        public void remove(Node node) {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-            node.next = null;
-            node.prev = null;
-        }
-    }
 
-    private static class Node {
-        private int key;
-        private int value;
-        private Node next;
-        private Node prev;
+        private static class Node {
+            int key;
+            int value;
+            Node prev;
+            Node next;
 
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-            this.next = null;
-            this.prev = null;
+            public Node(int key, int value) {
+                this.key = key;
+                this.value = value;
+                this.prev = null;
+                this.next = null;
+            }
         }
     }
 
