8a8624712526aaa541fd1c54aecf493c3613c7cd
rishabh kashyap <kashyap.rishab@hotmail.com>
2026-02-08 05:08:12 +0530

    Leetcode 69: square root

---
+++ algorithms-python/dnc/sqrt69.py
@@ -0,0 +1,26 @@
+class SquareRoot:
+    def get_square_root(self, num: int) -> int:
+        low = 0
+        high = num
+        ans = 0;
+        while low <= high:
+            mid = (low + high) // 2
+            if num == mid * mid:
+                return mid;
+            elif num > (mid * mid):
+                low = mid + 1
+                ans = mid
+            else:
+                high = mid - 1
+        return ans
+
+
+
+def main() -> None:
+    sqrt = SquareRoot()
+    print(sqrt.get_square_root(5))
+
+
+if __name__ == '__main__':
+    main()
+
