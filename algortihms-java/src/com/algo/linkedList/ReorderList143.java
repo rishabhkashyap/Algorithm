24b9867aa565acc9d30b9e1a3f5b9d31d59dd672
rishabh kashyap <kashyap.rishab@hotmail.com>
2026-02-09 08:11:09 +0530

    Adding explanation to the second solution

--- algortihms-java/src/com/algo/linkedList/ReorderList143.java
+++ algortihms-java/src/com/algo/linkedList/ReorderList143.java
@@ -57,6 +57,12 @@ public class ReorderList143 {
         }
     }
 
+    //For even size list like 1->2->3->4->5->6
+    //Middle part of the list(3 ->4) will remain same. Following solution therefore just
+    //reverse last 2 nodes of list ie 5->6. For any give even size linked list this solution
+    //will reverse nodes from n/2 +1 ....n. New head of reverse head will always be made to point to
+    //new head.next in merging loop, so it will always point to the  middle nodes(3->4) at the end and gives
+    //expected result.
     private static void reorderList2(Node head) {
         if (head == null || head.next == null) {
             return;
