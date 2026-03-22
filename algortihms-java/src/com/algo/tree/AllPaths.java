9f8250a5fa6cd7c7856ba3d1b23b884cb774b9eb
rishabh kashyap <kashyap.rishab@hotmail.com>
2026-03-22 11:40:57 +0530

    Adding iterative solution.

--- algortihms-java/src/com/algo/tree/AllPaths.java
+++ algortihms-java/src/com/algo/tree/AllPaths.java
@@ -2,10 +2,11 @@ package com.algo.tree;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class AllPaths {
 
-    public static void main(String[] args) {
+    static void main() {
         Node root = new Node(10);
         Node node8 = new Node(8);
         Node node2 = new Node(2);
@@ -19,7 +20,8 @@ public class AllPaths {
         node2.setLeft(node7);
         node7.setRight(new Node(22));
         findPaths(root);
-        System.out.println("\nPath to 23 exists = " + hasPath(root, 23));
+        System.out.println("\nPath to 23 exists = " + hasPath1(root, 23));
+        System.out.println("\nPath to 23 exists = " + hasPath2(root, 23));
 
     }
 
@@ -50,14 +52,40 @@ public class AllPaths {
     }
 
 
-    //Method return true if there exists a path whose sum is equal to
-    //give number
-    public static boolean hasPath(Node root, int sum) {
+    //Method return true if there exists a path whose sum is equal to given number
+    public static boolean hasPath1(Node root, int sum) {
         if (root == null)
             return false;
         if (root.getLeft() == null && root.getRight() == null && root.getData() == sum) {
             return true;
         }
-        return (hasPath(root.getLeft(), sum - root.getData()) || hasPath(root.getRight(), sum - root.getData()));
+        return (hasPath1(root.getLeft(), sum - root.getData()) || hasPath1(root.getRight(), sum - root.getData()));
     }
+
+    //Iterative method that return true if there exists a path whose sum is equal to given number
+    private static boolean hasPath2(Node root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        Stack<SumPath> stack = new Stack<>();
+        stack.push(new SumPath(root, root.getData()));
+        while (!stack.isEmpty()) {
+            var sumPath = stack.pop();
+            var node = sumPath.node;
+            var sum = sumPath.sum;
+            if (node.getLeft() == null && node.getRight() == null && sum == targetSum) {
+                return true;
+            }
+            if (node.getLeft() != null) {
+                stack.push(new SumPath(node.getLeft(), sum + node.getLeft().getData()));
+            }
+            if (node.getRight() != null) {
+                stack.push(new SumPath(node.getRight(), sum + node.getRight().getData()));
+            }
+        }
+        return false;
+    }
+
+    private record SumPath(Node node, int sum){}
+
 }
