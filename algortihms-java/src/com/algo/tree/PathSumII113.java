0e8f1eedc13ee15c68781e9bb11a77d8d3ba76cb
rishabh kashyap <kashyap.rishab@hotmail.com>
2026-03-23 08:39:47 +0530

    Leetcode113: Adding iterative solution

--- algortihms-java/src/com/algo/tree/PathSumII113.java
+++ algortihms-java/src/com/algo/tree/PathSumII113.java
@@ -1,12 +1,15 @@
 package com.algo.tree;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
 
 //Problem: https://leetcode.com/problems/path-sum-ii/description/
 
 public class PathSumII113 {
-    public static void main(String[] args) {
+    static void main() {
         var target = 14;
         var root = new TreeNode(3);
         var node5 = new TreeNode(5);
@@ -25,18 +28,21 @@ public class PathSumII113 {
         node1.right = node8;
         node2.left = node7;
         node2.right = node4;
-        var result = getPaths(root, target);
-        result.forEach(System.out::println);
+        var result = getPaths1(root, target);
+        System.out.println(result);
+        System.out.println();
+        result = getPaths2(root, target);
+        System.out.println(result);
     }
 
-    private static List<List<Integer>> getPaths(TreeNode root, int target) {
+    private static List<List<Integer>> getPaths1(TreeNode root, int target) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
-        getPaths(root, target, 0, path, result);
+        getPaths1(root, target, 0, path, result);
         return result;
     }
 
-    private static void getPaths(TreeNode root, int target, int sum, List<Integer> path,
+    private static void getPaths1(TreeNode root, int target, int sum, List<Integer> path,
                                   List<List<Integer>> result) {
         if (root == null) {
             return;
@@ -46,9 +52,40 @@ public class PathSumII113 {
         if (root.left == null && root.right == null && target == sum) {
             result.add(new ArrayList<>(path));
         }
-        getPaths(root.left, target, sum, path, result);
-        getPaths(root.right, target, sum, path, result);
-        path.remove(path.size() - 1);
+        getPaths1(root.left, target, sum, path, result);
+        getPaths1(root.right, target, sum, path, result);
+        path.removeLast();
+    }
+
+    private static List<List<Integer>> getPaths2(TreeNode root, int targetSum) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        Deque<PathSummary> stack = new ArrayDeque<>();
+        stack.push(new PathSummary(root, List.of(root.val), root.val));
+        List<List<Integer>> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            var summary = stack.pop();
+            var node = summary.node;
+            var path = summary.path;
+            if (summary.sum == targetSum && node.left == null && node.right == null) {
+                result.add(path);
+            }
+            if (node.left != null) {
+                List<Integer> list = new ArrayList<>(path);
+                list.add(node.left.val);
+                stack.push(new PathSummary(node.left, list, summary.sum + node.left.val));
+            }
+            if (node.right != null) {
+                List<Integer> list = new ArrayList<>(path);
+                list.add(node.right.val);
+                stack.push(new PathSummary(node.right, list, summary.sum + node.right.val));
+            }
+        }
+        return result;
+    }
+
+    private record PathSummary(TreeNode node, List<Integer> path, int sum) {
     }
 
     private static class TreeNode {
